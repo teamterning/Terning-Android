cb0f98cac295ee673b5f24687b00aac857d5e63e
Hyobeen-Park <amanda0109@naver.com>
2024-07-05 00:39:45 +0900

    [FEAT/#6] Color 추가

--- core/src/main/java/com/terning/core/designsystem/theme/Color.kt
+++ core/src/main/java/com/terning/core/designsystem/theme/Color.kt
@@ -9,3 +9,39 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Grey Scale
+val White = Color(0xFFFFFFFF)
+val Grey100 = Color(0xFFF5F5F5)
+val Grey150 = Color(0xFFE9E9E9)
+val Grey200 = Color(0xFFDDDDDD)
+val Grey300 = Color(0xFFBCBCBC)
+val Grey350 = Color(0xFFADADAD)
+val Grey400 = Color(0xFF666666)
+val Grey500 = Color(0xFF373737)
+val Black = Color(0xFF171717)
+
+// Main Color
+val TerningMain = Color(0xFF1EA65E)
+
+// Calendar Color
+val CalRed = Color(0xFFED4E54)
+
+val CalOrange1 = Color(0xFFEE7647)
+val CalOrange2 = Color(0xFF5397F3)
+
+val CalYellow = Color(0xFFF5E660)
+
+val CalGreen1 = Color(0xFFC4E953)
+val CalGreen2 = Color(0xFF84D558)
+
+val CalBlue1 = Color(0xFF45D0CC)
+val CalBlue2 = Color(0xFF4119F2)
+
+val CalPurple = Color(0xFF9B64E2)
+
+val CalPink = Color(0xFFF260AC)
+
+// Other
+val WarningRed = Color(0xFFF54645)
+val SundayRed = Color(0xFFEB1211)
