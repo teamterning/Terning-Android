c33d2c862fcb0a66e31292c010e5dab1fc5d1be5
Hyobeen-Park <amanda0109@naver.com>
2024-07-05 17:16:37 +0900

    [FEAT/#6] theme 설정

--- core/src/main/java/com/terning/core/designsystem/theme/Theme.kt
+++ core/src/main/java/com/terning/core/designsystem/theme/Theme.kt
@@ -1,14 +1,13 @@
 package com.terning.core.designsystem.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -32,26 +31,40 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+private val LocalTerningTypography = staticCompositionLocalOf<TerningTypography> {
+    error("No TerningTypography provided")
+}
+
+object TerningTheme {
+    val typography: TerningTypography
+        @Composable
+        get() = LocalTerningTypography.current
+}
+
 @Composable
-fun TerningAndroidTheme(
+fun ProvideTerningTypography(typography: TerningTypography, content: @Composable () -> Unit) {
+    val provideTypography = remember { typography.copy() }
+    provideTypography.update(typography)
+    CompositionLocalProvider(
+        LocalTerningTypography provides provideTypography,
+        content = content
+    )
+}
+
+@Composable
+fun TerningTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorScheme = LightColorScheme
+    val typography = TerningTypography()
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    ProvideTerningTypography(typography = typography) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content,
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
