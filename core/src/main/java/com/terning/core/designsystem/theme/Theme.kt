e0203538cbee704d020cbb2e81cd167e690ccff0
Hyobeen-Park <amanda0109@naver.com>
2024-07-05 17:39:42 +0900

    [CHORE/#6] 코드 정리

--- core/src/main/java/com/terning/core/designsystem/theme/Theme.kt
+++ core/src/main/java/com/terning/core/designsystem/theme/Theme.kt
@@ -2,33 +2,14 @@ package com.terning.core.designsystem.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = TerningMain,
 )
 
 private val LocalTerningTypography = staticCompositionLocalOf<TerningTypography> {
--- core/src/main/java/com/terning/core/designsystem/theme/Type.kt
+++ core/src/main/java/com/terning/core/designsystem/theme/Type.kt
@@ -1,6 +1,5 @@
 package com.terning.core.designsystem.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
@@ -18,33 +17,6 @@ val PretendardSemiBold = FontFamily(Font(R.font.pretendard_semibold))
 val PretendardMedium = FontFamily(Font(R.font.pretendard_medium))
 val PretendardRegular = FontFamily(Font(R.font.pretendard_regular))
 val PretendardLight = FontFamily(Font(R.font.pretendard_light))
-
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
 
 @Stable
 class TerningTypography internal constructor(
