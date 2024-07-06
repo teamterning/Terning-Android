ac8dd672952086bc6d3917663ccbd1c190180216
LEE YOU BIN <youbeene3409@naver.com>
2024-07-06 18:39:43 +0900

    [CHORE/#5] nonnull로 수정

--- feature/src/main/java/com/terning/feature/onboarding/signin/SignInState.kt
+++ feature/src/main/java/com/terning/feature/onboarding/signin/SignInState.kt
@@ -3,5 +3,5 @@ package com.terning.feature.onboarding.signin
 import com.terning.core.state.UiState
 
 data class SignInState (
-    val accessToken: UiState<String>? = UiState.Loading
+    val accessToken: UiState<String> = UiState.Loading
 )
