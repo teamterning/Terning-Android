4ec2e763209f2bbf9d11000a1591117338532314
Hyobeen-Park <amanda0109@naver.com>
2024-07-07 01:50:07 +0900

    [FEAT/#12] 홈뷰 스크랩 없을 때 오늘 마감 공고 뷰 구현

--- feature/src/main/java/com/terning/feature/home/HomeRoute.kt
+++ feature/src/main/java/com/terning/feature/home/HomeRoute.kt
@@ -4,18 +4,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.terning.core.designsystem.theme.Black
+import com.terning.core.designsystem.theme.Grey500
 import com.terning.core.designsystem.theme.TerningMain
 import com.terning.core.designsystem.theme.TerningTheme
+import com.terning.core.designsystem.theme.White
 import com.terning.feature.R
 
 @Composable
@@ -23,6 +31,7 @@ fun HomeRoute() {
     HomeScreen()
 }
 
+@Preview
 @Composable
 fun HomeScreen() {
     Column(
@@ -53,4 +62,24 @@ fun HomeScreen() {
             color = Black,
         )
     }
+}
+
+@Composable
+fun HomeNoScrapItem() {
+    Text(
+        text = stringResource(id = R.string.home_today_no_scrap),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(116.dp)
+            .padding(top = 19.dp)
+            .background(
+                color = White,
+                shape = RoundedCornerShape(5.dp)
+            )
+            .shadow(1.dp)
+            .wrapContentHeight(align = Alignment.CenterVertically),
+        textAlign = TextAlign.Center,
+        style = TerningTheme.typography.detail2,
+        color = Grey500,
+    )
 }
