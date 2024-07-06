d6db78757b7b69237ebbd3fa886f74629ea87213
Hyobeen-Park <amanda0109@naver.com>
2024-07-06 21:40:07 +0900

    [FEAT/#12] 오늘 마감 공고 아이템 구현

---
+++ feature/src/main/java/com/terning/feature/home/HomeClosingTodayItem.kt
@@ -0,0 +1,59 @@
+package com.terning.feature.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.terning.core.designsystem.theme.Grey150
+import com.terning.core.designsystem.theme.Grey500
+import com.terning.core.designsystem.theme.TerningTheme
+import com.terning.core.designsystem.theme.White
+
+@Composable
+fun HomeClosingTodayItem(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier
+            .background(White)
+            .width(140.dp)
+            .height(116.dp)
+            .border(
+                width = 1.dp,
+                color = Grey150,
+                RoundedCornerShape(5.dp),
+            ),
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Box(
+            modifier
+                .background(
+                    color = Grey500,
+                    shape = RoundedCornerShape(
+                        topStart = 5.dp,
+                        bottomStart = 5.dp
+                    )
+                )
+                .width(8.dp)
+                .fillMaxHeight()
+        )
+        Text(
+            text = title,
+            modifier
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 8.dp),
+            style = TerningTheme.typography.button3
+        )
+    }
+}
