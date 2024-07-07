a7f65c89b37792e220844474bf368aee053e96d9
boiledegg <tjrwns2163@naver.com>
2024-07-07 14:43:07 +0900

    [CHORE/#9] Import 정리

--- feature/src/main/java/com/terning/feature/calendar/CalendarDay.kt
+++ feature/src/main/java/com/terning/feature/calendar/CalendarDay.kt
@@ -5,9 +5,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
--- feature/src/main/java/com/terning/feature/calendar/WeekDaysHeader.kt
+++ feature/src/main/java/com/terning/feature/calendar/WeekDaysHeader.kt
@@ -1,11 +1,9 @@
 package com.terning.feature.calendar
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
