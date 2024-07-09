da62b7dce3b49e0e32771edab3ef23ec6746275d
boiledegg <tjrwns2163@naver.com>
2024-07-10 02:29:04 +0900

    [UI/#20] 주간 캘린더 UI 구현

---
+++ feature/src/main/java/com/terning/feature/calendar/CalendarWeek.kt
@@ -0,0 +1,50 @@
+package com.terning.feature.calendar
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.terning.feature.calendar.models.MonthData
+import java.time.LocalDate
+import java.time.YearMonth
+
+@Composable
+fun CalendarWeek(
+    modifier: Modifier = Modifier,
+    selectedDate: LocalDate,
+    onDateSelected: (LocalDate) -> Unit = {}
+) {
+    val monthData = MonthData(YearMonth.of(selectedDate.year, selectedDate.monthValue))
+    val currentWeek = selectedDate.dayOfMonth / 7
+
+    val pagerState = rememberPagerState (
+        initialPage = currentWeek,
+        pageCount = {monthData.totalDays / 7}
+    )
+
+    HorizontalPager(
+        modifier = modifier,
+        state = pagerState) { page ->
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            items(items = monthData.calendarMonth.weekDays[page]) { day ->
+                CalendarDay(
+                    dayData = day,
+                    isSelected = selectedDate == day.date,
+                    isToday = day.date == LocalDate.now(),
+                    onDateSelected = onDateSelected
+                )
+            }
+        }
+    }
+}
