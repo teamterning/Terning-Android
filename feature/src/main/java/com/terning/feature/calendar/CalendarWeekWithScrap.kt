9c70c38475d8ad852bc353486df140edfcfc1ab4
boiledegg <tjrwns2163@naver.com>
2024-07-10 02:29:38 +0900

    [UI/#20] 주간 캘린더 + 당일 스크랩 목록 표시 화면 구현

---
+++ feature/src/main/java/com/terning/feature/calendar/CalendarWeekWithScrap.kt
@@ -0,0 +1,59 @@
+package com.terning.feature.calendar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
+import com.terning.core.designsystem.theme.Grey200
+import com.terning.core.designsystem.theme.White
+import com.terning.feature.calendar.models.Scrap
+import java.time.LocalDate
+
+@Composable
+fun CalendarWeekWithScrap(
+    modifier: Modifier = Modifier,
+    selectedDate: LocalDate?,
+    scrapLists: List<List<Scrap>> = listOf(),
+    onDateSelected: (LocalDate) -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Card(
+            modifier = Modifier
+                .border(0.dp, Grey200, RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
+                .shadow(
+                    shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp),
+                    elevation = 1.dp
+                ),
+
+            shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp),
+        ) {
+            CalendarWeek(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(White),
+                selectedDate = selectedDate?: LocalDate.now(),
+                onDateSelected = onDateSelected
+            )
+        }
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+        ){
+            /*items(items = scrapLists[selectedDate?.dayOfMonth - 1]) {
+
+            }*/
+
+        }
+    }
+
+}
