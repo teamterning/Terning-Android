2e49faece424b975653e41ef0791c9e145af6d96
Hyobeen-Park <amanda0109@naver.com>
2024-07-10 05:40:12 +0900

    [FEAT/#12] 필터링 ui 구현

---
+++ feature/src/main/java/com/terning/feature/home/HomeFilteringScreen.kt
@@ -0,0 +1,112 @@
+package com.terning.feature.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.terning.core.designsystem.theme.Black
+import com.terning.core.designsystem.theme.Grey300
+import com.terning.core.designsystem.theme.TerningMain
+import com.terning.core.designsystem.theme.TerningTheme
+import com.terning.core.designsystem.theme.White
+import com.terning.feature.R
+
+@Composable
+fun HomeFilteringScreen(
+    grade: Int,
+    period: Int,
+    month: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .padding(top = 10.dp)
+            .height(IntrinsicSize.Min)
+    ) {
+        Row(
+            modifier = modifier
+                .background(
+                    color = TerningMain,
+                    shape = RoundedCornerShape(5.dp),
+                )
+                .align(Alignment.CenterVertically)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_home_filtering_28),
+                contentDescription = stringResource(id = R.string.home_recommend_filtering),
+                modifier = modifier
+                    .padding(horizontal = 2.dp),
+            )
+            Text(
+                text = stringResource(id = R.string.home_recommend_filtering),
+                style = TerningTheme.typography.button4,
+                color = White,
+                modifier = modifier
+                    .padding(vertical = 8.dp)
+                    .padding(end = 8.dp)
+            )
+        }
+
+        HomeFilteringText(
+            text = grade.toString() + stringResource(id = R.string.home_recommend_filtering_grade),
+            modifier = Modifier
+                .padding(vertical = 7.dp, horizontal = 28.dp),
+        )
+        HomeFilteringDivider()
+        HomeFilteringText(
+            text = stringResource(
+                id = when (period) {
+                    1 -> R.string.home_recommend_filtering_working_period_1
+                    2 -> R.string.home_recommend_filtering_working_period_2
+                    3 -> R.string.home_recommend_filtering_working_period_3
+                    else -> R.string.server_failure
+                }
+            ),
+            modifier = Modifier
+                .padding(vertical = 7.dp, horizontal = 25.dp)
+        )
+        HomeFilteringDivider()
+        HomeFilteringText(
+            text = month.toString() + stringResource(id = R.string.home_recommend_filtering_month),
+            modifier = Modifier
+                .padding(vertical = 7.dp)
+                .padding(start = 34.dp, end = 13.dp)
+        )
+    }
+}
+
+@Composable
+private fun HomeFilteringText(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        style = TerningTheme.typography.detail2,
+        color = Black,
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun HomeFilteringDivider() {
+    VerticalDivider(
+        color = Grey300,
+        thickness = 2.dp,
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(vertical = 4.dp),
+    )
+}
