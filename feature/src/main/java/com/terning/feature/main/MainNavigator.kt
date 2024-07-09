e7cf63f9aa28e827947336cf99d71c8414d465d4
arinming <jyandysys1@naver.com>
2024-07-09 23:30:30 +0900

    [FIX/#23] 경로 수정

--- feature/src/main/java/com/terning/feature/main/MainNavigator.kt
+++ feature/src/main/java/com/terning/feature/main/MainNavigator.kt
@@ -12,6 +12,7 @@ import androidx.navigation.navOptions
 import com.terning.feature.calendar.navigation.navigateCalendar
 import com.terning.feature.home.navigation.navigateHome
 import com.terning.feature.mypage.navigation.navigateMyPage
+import com.terning.feature.onboarding.signin.navigation.SignIn
 import com.terning.feature.search.navigation.Search
 import com.terning.feature.search.navigation.navigateSearch
 
@@ -22,7 +23,7 @@ class MainNavigator(
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
 
-    val startDestination = Search
+    val startDestination = SignIn
 
     val currentTab: MainTab?
         @Composable get() = MainTab.find { tab ->
