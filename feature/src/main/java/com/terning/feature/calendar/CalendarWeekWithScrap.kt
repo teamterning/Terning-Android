99513357e2fe8b5c30309bb40082369e1d04f106
boiledegg <tjrwns2163@naver.com>
2024-07-11 03:54:21 +0900

    [UI/#37] 주간 캘린더 스크랩 목록 화면 구현

--- feature/src/main/java/com/terning/feature/calendar/CalendarWeekWithScrap.kt
+++ feature/src/main/java/com/terning/feature/calendar/CalendarWeekWithScrap.kt
@@ -2,19 +2,30 @@ package com.terning.feature.calendar
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.terning.core.designsystem.theme.Black
 import com.terning.core.designsystem.theme.Grey200
+import com.terning.core.designsystem.theme.Grey400
+import com.terning.core.designsystem.theme.TerningTheme
 import com.terning.core.designsystem.theme.White
+import com.terning.core.extension.getDateStringInKorean
 import com.terning.feature.calendar.models.Scrap
 import com.terning.feature.calendar.models.SelectedDateState
 import java.time.LocalDate
@@ -34,7 +45,8 @@ fun CalendarWeekWithScrap(
                 .border(
                     width = 0.dp,
                     color = Grey200,
-                    shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
+                    shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp)
+                )
                 .shadow(
                     shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp),
                     elevation = 1.dp
@@ -52,19 +64,40 @@ fun CalendarWeekWithScrap(
         }
 
         Text(
-            text = LocalDate.now().toString()
+            text = selectedDate.selectedDate.getDateStringInKorean(),
+            style = TerningTheme.typography.title5,
+            color = Black,
+            modifier = Modifier.padding(start = 24.dp, top = 16.dp, bottom = 15.dp)
         )
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            items(items = scrapLists[selectedDate.selectedDate.dayOfMonth - 1]) { scrap ->
-                CalendarScrap(
-                    scrap = scrap
-                )
+        if (scrapLists[selectedDate.selectedDate.dayOfMonth - 1].isEmpty()) {
+            Text(
+                modifier = Modifier
+                    .padding(top = 42.dp)
+                    .fillMaxWidth(),
+                text = "선택하신 날짜에 지원 마감인 스크랩 공고가 없어요.",
+                textAlign = TextAlign.Center,
+                style = TerningTheme.typography.body5,
+                color = Grey400
+            )
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+            ) {
+                items(items = scrapLists[selectedDate.selectedDate.dayOfMonth - 1]) { scrap ->
+                    CalendarScrap(
+                        scrap = scrap
+                    )
+                    Spacer(
+                        modifier = Modifier.height(12.dp)
+                    )
+                }
             }
         }
     }
-
 }
+
+
+
