efe9edd2fa53087ed5681860eadd63273a3d6dc4
boiledegg <tjrwns2163@naver.com>
2024-07-11 01:24:52 +0900

    [FEAT/#37] LocalDate 확장함수 추가

--- core/src/main/java/com/terning/core/extension/LocalDateExt.kt
+++ core/src/main/java/com/terning/core/extension/LocalDateExt.kt
@@ -1,9 +1,14 @@
 package com.terning.core.extension
 
 import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.Locale
 
 fun LocalDate.getStringAsTitle(): String =
     "${year}년 ${monthValue.toString().padStart(2, '0')}월"
+
+fun LocalDate.getDateStringInKorean(): String =
+    "${monthValue}월 ${dayOfMonth}일 ${dayOfWeek.getDisplayName(TextStyle.FULL, Locale.KOREAN)}"
 
 fun LocalDate.getWeekIndexContainingSelectedDate(): Int = dayOfMonth / 7
 
