ee0ca25ef4a4faf4240a175a2b5493ce9424f550
LEE YOU BIN <youbeene3409@naver.com>
2024-07-11 07:12:51 +0900

    [FEAT/#14] startDestination 수정

--- feature/src/main/java/com/terning/feature/main/MainNavigator.kt
+++ feature/src/main/java/com/terning/feature/main/MainNavigator.kt
@@ -13,8 +13,6 @@ import com.terning.feature.calendar.navigation.navigateCalendar
 import com.terning.feature.home.navigation.navigateHome
 import com.terning.feature.mypage.navigation.navigateMyPage
 import com.terning.feature.onboarding.signin.navigation.SignIn
-import com.terning.feature.onboarding.signup.navigation.SignUp
-import com.terning.feature.search.navigation.Search
 import com.terning.feature.search.navigation.navigateSearch
 
 class MainNavigator(
@@ -24,7 +22,7 @@ class MainNavigator(
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
 
-    val startDestination = SignUp
+    val startDestination = SignIn
 
     val currentTab: MainTab?
         @Composable get() = MainTab.find { tab ->
