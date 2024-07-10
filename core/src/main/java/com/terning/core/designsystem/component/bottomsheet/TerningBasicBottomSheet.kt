92fc9713e5a1eab18e80cc7dfa8e239ae60196d9
Hyobeen-Park <amanda0109@naver.com>
2024-07-11 03:17:28 +0900

    [CHORE/#32] TerningBasicBottomSheet 파라미터 추가 및 어노테이션 추가

--- core/src/main/java/com/terning/core/designsystem/component/bottomsheet/TerningBasicBottomSheet.kt
+++ core/src/main/java/com/terning/core/designsystem/component/bottomsheet/TerningBasicBottomSheet.kt
@@ -1,27 +1,27 @@
 package com.terning.core.designsystem.component.bottomsheet
 
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.terning.core.designsystem.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TerningBasicBottomSheet(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
-    onDismissRequest: () -> Unit
+    onDismissRequest: () -> Unit,
+    sheetState: SheetState = rememberModalBottomSheetState()
 ) {
-    val sheetState = rememberModalBottomSheetState()
-
     ModalBottomSheet(
         onDismissRequest = {
             onDismissRequest()
         },
         sheetState = sheetState,
-        modifier = modifier.navigationBarsPadding()
+        containerColor = White,
     ) {
         content()
     }
