d1b435d9b50dc5cb72c6a44b5e824a8e1360ee31
LEE YOU BIN <youbeene3409@naver.com>
2024-07-12 04:37:00 +0900

    [FEAT/#48] Text 구현

--- core/src/main/java/com/terning/core/designsystem/component/image/TerningImage.kt
+++ core/src/main/java/com/terning/core/designsystem/component/image/TerningImage.kt
@@ -2,6 +2,7 @@ package com.terning.core.designsystem.component.image
 
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -9,12 +10,14 @@ import com.terning.core.R
 import com.terning.core.designsystem.theme.TerningPointTheme
 
 @Composable
-fun TerningBasicImage(
-    painter: Int
+fun TerningImage(
+    painter: Int,
+    modifier: Modifier = Modifier
 ) {
     Image(
         painter = painterResource(id = painter),
-        contentDescription = stringResource(id = R.string.image_content_descriptin)
+        contentDescription = stringResource(id = R.string.image_content_descriptin),
+        modifier = modifier
     )
 }
 
@@ -22,7 +25,7 @@ fun TerningBasicImage(
 @Composable
 fun TerningBasicImagePreview() {
     TerningPointTheme {
-        TerningBasicImage(
+        TerningImage(
             painter = R.drawable.ic_back
         )
     }
--- feature/src/main/java/com/terning/feature/onboarding/filtering/FilteringOneScreen.kt
+++ feature/src/main/java/com/terning/feature/onboarding/filtering/FilteringOneScreen.kt
@@ -4,12 +4,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.terning.core.designsystem.component.image.TerningImage
 import com.terning.core.designsystem.component.topappbar.BackButtonTopAppBar
-import com.terning.feature.onboarding.filtering.component.FilteringStatus
+import com.terning.core.designsystem.theme.TerningTheme
+import com.terning.feature.R
 
 @Composable
 fun FilteringOneScreen(
@@ -17,6 +22,7 @@ fun FilteringOneScreen(
     modifier: Modifier = Modifier,
     filteringViewModel: FilteringViewModel = hiltViewModel()
 ) {
+    val name: String = "남지우"
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -30,9 +36,29 @@ fun FilteringOneScreen(
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-            FilteringStatus()
+            TerningImage(
+                painter = R.drawable.ic_filtering_status1,
+                modifier = modifier.padding(
+                    top = 20.dp,
+                    start = 24.dp
+                )
+            )
+            Text(
+                text = stringResource(id = R.string.filtering_title, name),
+                style = TerningTheme.typography.title3,
+                modifier = modifier.padding(
+                    top = 19.dp,
+                    start = 24.dp
+                )
+            )
+            Text(
+                text = stringResource(id = R.string.filtering_sub, name),
+                style = TerningTheme.typography.body5,
+                modifier = modifier.padding(
+                    top = 3.dp,
+                    start = 24.dp
+                )
+            )
         }
     }
 }
-
-
