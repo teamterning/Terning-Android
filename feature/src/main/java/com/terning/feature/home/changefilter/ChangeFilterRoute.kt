603d2eb0f2f6ef30946f6fcdd70ce893a8935c3c
Hyobeen-Park <amanda0109@naver.com>
2024-07-12 05:27:10 +0900

    [FEAT/#51] 탑바 적용

--- feature/src/main/java/com/terning/feature/home/changefilter/ChangeFilterRoute.kt
+++ feature/src/main/java/com/terning/feature/home/changefilter/ChangeFilterRoute.kt
@@ -1,17 +1,41 @@
 package com.terning.feature.home.changefilter
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.terning.core.designsystem.component.topappbar.BackButtonTopAppBar
+import com.terning.feature.R
 
 @Composable
 fun ChangeFilterRoute(
     navController: NavController,
 ) {
-    ChangeFilterScreen()
+    ChangeFilterScreen(navController)
 }
 
 @Composable
-fun ChangeFilterScreen() {
-    Text(text = "필터링 재설정")
+fun ChangeFilterScreen(navController: NavController) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            BackButtonTopAppBar(
+                title = stringResource(id = R.string.change_filter_top_bar_title),
+                onBackButtonClick = { navController.popBackStack() },
+                modifier = Modifier
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+            Text(text = "dd")
+        }
+
+    }
 }
