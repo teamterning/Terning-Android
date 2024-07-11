0c6386469dc7bf9a7a1a3319a112feb35401dbb1
arinming <jyandysys1@naver.com>
2024-07-12 00:37:31 +0900

    [FEAT/#42] 공고 상세 페이지 상단 텍스트 구현

--- feature/src/main/java/com/terning/feature/intern/InternRoute.kt
+++ feature/src/main/java/com/terning/feature/intern/InternRoute.kt
@@ -14,6 +14,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.terning.core.designsystem.component.topappbar.BackButtonTopAppBar
+import com.terning.core.designsystem.theme.Black
+import com.terning.core.designsystem.theme.Grey200
+import com.terning.core.designsystem.theme.TerningMain
+import com.terning.core.designsystem.theme.TerningTheme
+import com.terning.core.extension.customShadow
 import com.terning.core.extension.noRippleClickable
 import com.terning.feature.R
 
@@ -36,7 +41,10 @@ fun InternScreen(
         topBar = {
             BackButtonTopAppBar(
                 title = "공고 정보",
-                modifier = Modifier,
+                modifier = Modifier.customShadow(
+                    color = Grey200,
+                    offsetY = 2.dp
+                ),
                 onBackButtonClick = {},
                 listOf(
                     {},
@@ -44,7 +52,9 @@ fun InternScreen(
                         IconButton(onClick = {}) {
                             Icon(
                                 painter = painterResource(id = R.drawable.ic_intern_share_22),
-                                contentDescription = stringResource(id = R.string.intern_share_icon),
+                                contentDescription = stringResource(
+                                    id = R.string.intern_share_icon
+                                ),
                                 modifier = modifier
                                     .noRippleClickable { }
                             )
@@ -58,8 +68,19 @@ fun InternScreen(
         Column(
             modifier = modifier
                 .padding(paddingValues)
+                .padding(24.dp)
         ) {
-            Text(text = "공고 정보")
+            Text(
+                text = "D-3",
+                style = TerningTheme.typography.body0,
+                color = TerningMain
+            )
+            Text(
+                text = "[NAVER Cloud] 의료 특화 초거대 언어모델 학습 데이터 구축 및 안전성 평가 업무 (체험형 인턴)",
+                style = TerningTheme.typography.title2,
+                color = Black,
+                modifier = modifier.padding(top = 8.dp)
+            )
         }
     }
 }
