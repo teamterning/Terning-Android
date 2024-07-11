215a7ce6a37ea840df989d953b5ea8e61b3a33b0
arinming <jyandysys1@naver.com>
2024-07-12 01:00:47 +0900

    [FEAT/#42] 공고 상세 정보 컴포넌트 구현

--- feature/src/main/java/com/terning/feature/intern/InternRoute.kt
+++ feature/src/main/java/com/terning/feature/intern/InternRoute.kt
@@ -1,13 +1,19 @@
 package com.terning.feature.intern
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -16,6 +22,7 @@ import androidx.navigation.NavHostController
 import com.terning.core.designsystem.component.topappbar.BackButtonTopAppBar
 import com.terning.core.designsystem.theme.Black
 import com.terning.core.designsystem.theme.Grey200
+import com.terning.core.designsystem.theme.Grey500
 import com.terning.core.designsystem.theme.TerningMain
 import com.terning.core.designsystem.theme.TerningTheme
 import com.terning.core.extension.customShadow
@@ -60,7 +67,9 @@ fun InternScreen(
                             )
                         }
                     },
-                    { Spacer(modifier = modifier.padding(end = 8.dp)) }
+                    {
+                        Spacer(modifier = modifier.padding(end = 8.dp))
+                    }
                 )
             )
         }
@@ -79,8 +88,52 @@ fun InternScreen(
                 text = "[NAVER Cloud] 의료 특화 초거대 언어모델 학습 데이터 구축 및 안전성 평가 업무 (체험형 인턴)",
                 style = TerningTheme.typography.title2,
                 color = Black,
-                modifier = modifier.padding(top = 8.dp)
+                modifier = modifier.padding(
+                    top = 8.dp,
+                    bottom = 16.dp
+                )
             )
+            Column(
+                modifier = modifier
+                    .border(
+                        width = 1.dp,
+                        color = TerningMain,
+                        shape = RoundedCornerShape(size = 5.dp)
+                    )
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 17.dp, bottom = 15.dp),
+                verticalArrangement = Arrangement.spacedBy(6.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.Start,
+            ) {
+                val internInfo = listOf(
+                    "서류 마감" to "2024년 7월 23일",
+                    "근무 기간" to "2개월",
+                    "근무 시작" to "2024년 8월"
+                )
+                internInfo.forEach { (title, value) ->
+                    InternInfoRow(title, value)
+                }
+            }
+            Text(text = "조회수 3,")
         }
+    }
+}
+
+@Composable
+fun InternInfoRow(title: String, value: String) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.Start),
+        verticalAlignment = Alignment.Top,
+    ) {
+        Text(
+            text = title,
+            style = TerningTheme.typography.body2,
+            color = Black,
+        )
+        Text(
+            text = value,
+            style = TerningTheme.typography.body3,
+            color = Grey500,
+        )
     }
 }
