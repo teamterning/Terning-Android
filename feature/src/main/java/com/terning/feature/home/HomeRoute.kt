c4dfed424a0a826858cd9974545d5ad3514f564f
Hyobeen-Park <amanda0109@naver.com>
2024-07-11 22:20:48 +0900

    [CHORE/#12] conflict 수정

--- feature/src/main/java/com/terning/feature/home/HomeRoute.kt
+++ feature/src/main/java/com/terning/feature/home/HomeRoute.kt
@@ -8,13 +8,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.terning.core.designsystem.component.topappbar.LogoTopAppBar
 import com.terning.core.designsystem.theme.Black
 import com.terning.core.designsystem.theme.Grey150
 import com.terning.core.designsystem.theme.TerningTheme
@@ -22,7 +22,6 @@ import com.terning.core.designsystem.theme.White
 import com.terning.feature.R
 import com.terning.feature.home.component.HomeFilteringScreen
 import com.terning.feature.home.component.HomeTodayIntern
-import com.terning.core.designsystem.component.topappbar.LogoTopAppBar
 
 @Composable
 fun HomeRoute() {
@@ -38,81 +37,77 @@ fun HomeScreen() {
             LogoTopAppBar()
         }
     ) { paddingValues ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
+                .padding(top = paddingValues.calculateTopPadding())
         ) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                item {
-                    Column(
+            item {
+                Column(
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                ) {
+                    Text(
+                        text = stringResource(
+                            id = R.string.home_today_title_start
+                        )
+                                + "남지우"
+                                + stringResource(
+                            id = R.string.home_today_title_end
+                        ),
                         modifier = Modifier
-                            .padding(bottom = 16.dp)
-                    ) {
-                        Text(
-                            text = stringResource(
-                                id = R.string.home_today_title_start
-                            )
-                                    + "남지우"
-                                    + stringResource(
-                                id = R.string.home_today_title_end
-                            ),
-                            modifier = Modifier
-                                .padding(top = 11.dp)
-                                .padding(horizontal = 24.dp),
-                            style = TerningTheme.typography.title1,
-                            color = Black,
-                        )
-                        HomeTodayIntern()
-                    }
+                            .padding(top = 11.dp)
+                            .padding(horizontal = 24.dp),
+                        style = TerningTheme.typography.title1,
+                        color = Black,
+                    )
+                    HomeTodayIntern()
                 }
-                stickyHeader {
-                    Column(
+            }
+            stickyHeader {
+                Column(
+                    modifier = Modifier
+                        .background(White)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.home_recommend_sub_title),
+                        style = TerningTheme.typography.detail2,
+                        color = Black,
                         modifier = Modifier
-                            .background(White)
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.home_recommend_sub_title),
-                            style = TerningTheme.typography.detail2,
-                            color = Black,
-                            modifier = Modifier
-                                .padding(top = 25.dp)
-                                .padding(horizontal = 24.dp),
-                        )
+                            .padding(top = 25.dp)
+                            .padding(horizontal = 24.dp),
+                    )
 
-                        Text(
-                            text = stringResource(id = R.string.home_recommend_main_title),
-                            style = TerningTheme.typography.title1,
-                            color = Black,
-                            modifier = Modifier
-                                .padding(top = 5.dp)
-                                .padding(horizontal = 24.dp),
-                        )
+                    Text(
+                        text = stringResource(id = R.string.home_recommend_main_title),
+                        style = TerningTheme.typography.title1,
+                        color = Black,
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                            .padding(horizontal = 24.dp),
+                    )
 
-                        HomeFilteringScreen(3, 1, 7)
+                    HomeFilteringScreen(3, 1, 7)
 
-                        HorizontalDivider(
-                            thickness = 4.dp,
-                            color = Grey150,
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                        )
-                    }
-                }
-
-                items(10) {
-                    TerningPostItem(
-                        imageUrl = "https://reqres.in/img/faces/7-image.jpg",
-                        title = "[Someone] 콘텐츠 마케터 대학생 인턴 채용",
-                        dateDeadline = "2",
-                        workingPeriod = "2개월",
-                        isScraped = false,
+                    HorizontalDivider(
+                        thickness = 4.dp,
+                        color = Grey150,
+                        modifier = Modifier
+                            .fillMaxWidth(),
                     )
                 }
             }
+
+            items(10) {
+                TerningPostItem(
+                    imageUrl = "https://reqres.in/img/faces/7-image.jpg",
+                    title = "[Someone] 콘텐츠 마케터 대학생 인턴 채용",
+                    dateDeadline = "2",
+                    workingPeriod = "2개월",
+                    isScraped = false,
+                )
+            }
         }
+
     }
 }
