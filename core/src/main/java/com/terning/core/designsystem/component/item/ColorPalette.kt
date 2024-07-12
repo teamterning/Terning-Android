4103230732e635bbd32658272bbc98b851407ca1
boiledegg <tjrwns2163@naver.com>
2024-07-12 16:37:15 +0900

    [REFACTOR/#57] 임포트 정리

--- core/src/main/java/com/terning/core/designsystem/component/item/ColorPalette.kt
+++ core/src/main/java/com/terning/core/designsystem/component/item/ColorPalette.kt
@@ -1,14 +1,10 @@
 package com.terning.core.designsystem.component.item
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Check
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
