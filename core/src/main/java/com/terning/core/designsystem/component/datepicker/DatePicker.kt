08ad2bb2d67763f34a6dc157a9a7e26d7424f146
LEE YOU BIN <youbeene3409@naver.com>
2024-07-13 03:28:54 +0900

    [FEAT/#48] Calendar 구현

--- core/src/main/java/com/terning/core/designsystem/component/datepicker/DatePicker.kt
+++ core/src/main/java/com/terning/core/designsystem/component/datepicker/DatePicker.kt
@@ -1,13 +1,10 @@
 package com.terning.core.designsystem.component.datepicker
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,178 +13,39 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
-import okhttp3.internal.immutableListOf
-import com.terning.core.R
-import com.terning.core.designsystem.theme.Black
-import com.terning.core.designsystem.theme.CalPink
-import com.terning.core.designsystem.theme.CalPurple
-import com.terning.core.designsystem.theme.Grey150
-import com.terning.core.designsystem.theme.TerningPointTheme
-import com.terning.core.designsystem.theme.White
-import com.terning.core.extension.noRippleClickable
+import com.terning.core.designsystem.theme.Grey300
+import com.terning.core.designsystem.theme.Grey500
+import com.terning.core.designsystem.theme.TerningMain
+import com.terning.core.designsystem.theme.TerningTheme
 import okhttp3.internal.toImmutableList
 
-@Composable
-fun DatePicker(
-    value: String,
-    placeHolder: String,
-    borderColor: Color,
-    isEditable: Boolean,
-    onClicked: () -> Unit = {},
-    modifier: Modifier = Modifier,
-) {
-    val isEmpty by remember(value) { derivedStateOf { value.isEmpty() } }
-
-    val newModifier = modifier
-        .fillMaxWidth()
-        .defaultMinSize(minHeight = 39.dp)
-        .clip(RoundedCornerShape(9.dp))
-        .then(
-            if (isEmpty || !isEditable) {
-                Modifier
-            } else {
-                Modifier.border(
-                    width = 1.dp,
-                    color = borderColor,
-                    shape = RoundedCornerShape(10.dp)
-                )
-            }
-        )
-
-    val backgroundColor = remember(isEmpty, isEditable) {
-        if (isEmpty || !isEditable) {
-            Grey150
-        } else {
-            White
-        }
-    }
-
-    Box(
-        modifier = newModifier
-            .background(backgroundColor, RoundedCornerShape(9.dp))
-            .noRippleClickable { if (isEditable) onClicked() }
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(horizontal = 14.dp, vertical = 10.dp)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                modifier = Modifier.padding(),
-                text = if (isEmpty) placeHolder else value,
-                color = if (isEmpty) Color.Red else Color.Blue,
-            )
-            Icon(
-                imageVector = ImageVector.vectorResource(id = R.drawable.ic_right),
-                contentDescription = "date picker icon",
-                tint = Color.Green
-            )
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun DataPickerBottomSheet(onSelected: (String) -> Unit, onDismissRequest: () -> Unit) {
-    val sheetState = rememberModalBottomSheetState()
-
-    val formatter = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
-    var chosenYear by remember { mutableIntStateOf(currentYear) }
-    var chosenMonth by remember { mutableIntStateOf(currentMonth) }
-    var chosenDay by remember { mutableIntStateOf(currentDay) }
-
-    val isValidDate by remember { derivedStateOf { calculateValidDate(chosenYear, chosenMonth, chosenDay) } }
-
-    ModalBottomSheet(
-        onDismissRequest = onDismissRequest,
-        sheetState = sheetState
-    ) {
-        DatePickerUI(
-            isValidDate = isValidDate,
-            chosenYear = chosenYear,
-            chosenMonth = chosenMonth,
-            chosenDay = chosenDay,
-            onYearChosen = { chosenYear = it },
-            onMonthChosen = { chosenMonth = it },
-            onDayChosen = { chosenDay = it }
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-        Button(
-            modifier = Modifier
-                .padding(horizontal = 20.dp)
-                .fillMaxWidth()
-                .background(
-                    color = if (isValidDate) Black else Color.Red,
-                    shape = RoundedCornerShape(9.dp)
-                ),
-            onClick =  {
-                if (isValidDate) {
-                    val calendar = Calendar.getInstance().apply {
-                        set(chosenYear, chosenMonth - 1, chosenDay)
-                    }
-                    val formattedDate = formatter.format(calendar.time)
-
-                    onSelected(formattedDate)
-                }
-            }
-        )
-        {
-            Text(text = "확인")
-        }
-        Spacer(modifier = Modifier.height(60.dp))
-    }
-}
-
-fun calculateValidDate(year: Int, month: Int, day: Int): Boolean {
-    val givenCalendar = Calendar.getInstance().apply {
-        set(year, month - 1, day)
-    }
-    val referenceCalendar = Calendar.getInstance()
-
-    return !givenCalendar.after(referenceCalendar)
-}
+private const val START_YEAR = 2023
+private const val END_YEAR = 2025
+private val years =
+    (listOf("") + (START_YEAR..END_YEAR).map { it.toString() } + listOf("")).toImmutableList()
+private val monthsNumber =
+    (listOf("") + (1..12).map { it.toString() } + listOf("")).toImmutableList()
 
 @Composable
 fun DatePickerUI(
-    isValidDate: Boolean,
     chosenYear: Int,
     chosenMonth: Int,
-    chosenDay: Int,
-    onYearChosen: (Int) -> Unit,
-    onMonthChosen: (Int) -> Unit,
-    onDayChosen: (Int) -> Unit,
+    onYearChosen: (Int) -> Unit = {},
+    onMonthChosen: (Int) -> Unit = {},
 ) {
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -195,66 +53,41 @@ fun DatePickerUI(
             .padding(vertical = 10.dp, horizontal = 5.dp)
     ) {
         DateSelectionSection(
-            isValidDate = isValidDate,
             chosenYear = chosenYear,
             chosenMonth = chosenMonth,
-            chosenDay = chosenDay,
             onYearChosen = { onYearChosen(it.toInt()) },
             onMonthChosen = { onMonthChosen(it.toInt()) },
-            onDayChosen = { onDayChosen(it.toInt()) },
         )
     }
 }
 
 @Composable
 fun DateSelectionSection(
-    isValidDate: Boolean,
     chosenYear: Int,
     chosenMonth: Int,
-    chosenDay: Int,
     onYearChosen: (String) -> Unit,
     onMonthChosen: (String) -> Unit,
-    onDayChosen: (String) -> Unit,
 ) {
     Row(
         horizontalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxWidth()
     ) {
         DateItemsPicker(
-            isValidDate = isValidDate,
-            max = currentYear - 1950,
             items = years.toImmutableList(),
             firstIndex = (chosenYear - START_YEAR),
             onItemSelected = onYearChosen
         )
         Spacer(modifier = Modifier.width(10.dp))
         DateItemsPicker(
-            isValidDate = isValidDate,
-            max = currentMonth,
             items = monthsNumber.toImmutableList(),
             firstIndex = chosenMonth,
             onItemSelected = onMonthChosen
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        DateItemsPicker(
-            isValidDate = isValidDate,
-            max = currentDay - 1,
-            items = when {
-                (chosenYear % 4 == 0) && chosenMonth == 2 -> days29.toImmutableList()
-                chosenMonth == 2 -> days28.toImmutableList()
-                days30Months.contains(chosenMonth) -> days30.toImmutableList()
-                else -> days31.toImmutableList()
-            },
-            firstIndex = chosenDay - 1,
-            onItemSelected = onDayChosen
         )
     }
 }
 
 @Composable
 fun DateItemsPicker(
-    isValidDate: Boolean,
-    max: Int,
     items: List<String>,
     firstIndex: Int,
     onItemSelected: (String) -> Unit,
@@ -263,16 +96,9 @@ fun DateItemsPicker(
     val listState = rememberLazyListState(firstIndex)
     val currentValue = remember { mutableStateOf("") }
 
-    LaunchedEffect(!listState.isScrollInProgress, isValidDate) {
-        if (isValidDate) {
-            onItemSelected(currentValue.value)
-            listState.animateScrollToItem(index = listState.firstVisibleItemIndex)
-        } else {
-            if (max < currentValue.value.toInt()) {
-                listState.animateScrollToItem(index = max)
-                onItemSelected(currentValue.value)
-            }
-        }
+    LaunchedEffect(!listState.isScrollInProgress) {
+        onItemSelected(currentValue.value)
+        listState.animateScrollToItem(index = listState.firstVisibleItemIndex)
     }
 
     Box(
@@ -285,11 +111,11 @@ fun DateItemsPicker(
         ) {
             HorizontalDivider(
                 modifier = Modifier.size(height = 1.dp, width = 60.dp),
-                color = Black
+                color = TerningMain
             )
             HorizontalDivider(
                 modifier = Modifier.size(height = 1.dp, width = 60.dp),
-                color = Black
+                color = TerningMain
             )
         }
         LazyColumn(
@@ -305,72 +131,15 @@ fun DateItemsPicker(
                 if (it == firstVisibleItemIndex + 1) {
                     currentValue.value = items[index]
                 }
-
                 Spacer(modifier = Modifier.height(6.dp))
-
                 Text(
                     text = items[index],
-                    modifier = Modifier.alpha(if (it == firstVisibleItemIndex + 1) 1f else 0.3f),
+                    style = TerningTheme.typography.title3,
+                    color = if (it == firstVisibleItemIndex + 1) Grey500 else Grey300,
                     textAlign = TextAlign.Center
                 )
-
                 Spacer(modifier = Modifier.height(6.dp))
             }
         }
     }
-}
-
-private val currentYear = Calendar.getInstance().get(Calendar.YEAR)
-private val currentMonth = Calendar.getInstance().get(Calendar.MONTH)
-private val currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-
-private const val START_YEAR = 1950
-private const val END_YEAR = 2100
-private val years = (listOf("") + (START_YEAR..END_YEAR).map { it.toString() } + listOf("")).toImmutableList()
-private val monthsNumber = (listOf("") + (1..12).map { it.toString() } + listOf("")).toImmutableList()
-private val days28 = (listOf("") + (1..28).map { it.toString() } + listOf("")).toImmutableList()
-private val days29 = (listOf("") + (1..29).map { it.toString() } + listOf("")).toImmutableList()
-private val days30 = (listOf("") + (1..30).map { it.toString() } + listOf("")).toImmutableList() // 4,6,9,11
-private val days31 = (listOf("") + (1..31).map { it.toString() } + listOf("")).toImmutableList() // 1,3,5,7,8,10.12
-private val days30Months = immutableListOf(4, 6, 9, 11)
-
-//@@Preview(showBackground = true)
-//@Composable
-//private fun DatePickerPreview() {
-//    TerningPointTheme {
-//        DatePicker(
-//            value = "2024.12.25",
-//            onClicked = {},
-//            borderColor = getLevelTextColor(2),
-//            placeHolder = "날짜를 입력해주세요.",
-//            isEditable = true
-//        )
-//    }
-//}
-
-//@Preview(showBackground = true)
-//@Composable
-//private fun CustomDatePickerPreview() {
-//    val chosenYear = remember { mutableIntStateOf(currentYear) }
-//    val chosenMonth = remember { mutableIntStateOf(currentMonth) }
-//    val chosenDay = remember { mutableIntStateOf(currentDay) }
-//    TerningPointTheme {
-//        DatePickerUI(
-//            isValidDate = true,
-//            chosenYear = chosenYear.value,
-//            chosenMonth = chosenMonth.value,
-//            chosenDay = chosenDay.value,
-//            onYearChosen = { chosenYear.value = it },
-//            onMonthChosen = { chosenMonth.value = it },
-//            onDayChosen = { chosenDay.value = it }
-//        )
-//    }
-//}
-
-@Composable
-fun getLevelTextColor(rank: Int) = when (rank) {
-    1 -> CalPink
-    2 -> CalPurple
-    3 -> Color.Green
-    else -> Color.Red
 }
--- feature/src/main/java/com/terning/feature/onboarding/filtering/FilteringThreeScreen.kt
+++ feature/src/main/java/com/terning/feature/onboarding/filtering/FilteringThreeScreen.kt
@@ -1,6 +1,5 @@
 package com.terning.feature.onboarding.filtering
 
-import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,21 +7,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.terning.core.designsystem.component.button.RectangleButton
-import com.terning.core.designsystem.component.datepicker.DatePicker
+import com.terning.core.designsystem.component.datepicker.DatePickerUI
 import com.terning.core.designsystem.component.image.TerningImage
 import com.terning.core.designsystem.component.topappbar.BackButtonTopAppBar
-import com.terning.core.designsystem.theme.TerningMain
 import com.terning.core.designsystem.theme.TerningTheme
 import com.terning.feature.R
 import com.terning.feature.onboarding.filtering.navigation.navigateFilteringThree
+import java.util.Calendar
 
 @Composable
 fun FilteringThreeScreen(
@@ -31,7 +33,10 @@ fun FilteringThreeScreen(
     filteringViewModel: FilteringViewModel = hiltViewModel(),
     onButtonClick: () -> Unit = {},
 ) {
-    val isButtonValid = remember { mutableStateOf(false) }
+    var chosenYear by remember { mutableIntStateOf(Calendar.getInstance().get(Calendar.YEAR)) }
+    var chosenMonth by remember { mutableIntStateOf(Calendar.getInstance().get(Calendar.MONTH)) }
+
+    val isDateMove by remember { mutableStateOf(false) }
 
     Scaffold(
         modifier = modifier,
@@ -70,20 +75,20 @@ fun FilteringThreeScreen(
                     bottom = 25.dp
                 )
             )
-            DatePicker(
-                value = "value",
-                placeHolder= "placeHolder",
-                borderColor = TerningMain,
-                isEditable=  false,
+            DatePickerUI(
+                chosenYear = chosenYear,
+                chosenMonth = chosenMonth,
+                onYearChosen = { chosenYear = it },
+                onMonthChosen = { chosenMonth = it },
             )
             Spacer(modifier = modifier.weight(1f))
             RectangleButton(
                 style = TerningTheme.typography.button0,
+                isEnabled = isDateMove,
                 paddingVertical = 25.dp,
                 text = R.string.filtering_button,
                 onButtonClick = { navController.navigateFilteringThree() },
                 modifier = modifier.padding(bottom = 12.dp),
-                isEnabled = isButtonValid.value
             )
         }
     }
