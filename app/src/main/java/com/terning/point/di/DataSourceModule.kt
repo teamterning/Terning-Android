b6b97b4bc19a3ed28dd97d842a805fd412bf9c64
boiledegg <tjrwns2163@naver.com>
2024-07-14 04:31:45 +0900

    [ADD/#59] 힐트 모듈 코드 추가

--- app/src/main/java/com/terning/point/di/DataSourceModule.kt
+++ app/src/main/java/com/terning/point/di/DataSourceModule.kt
@@ -1,7 +1,11 @@
 package com.terning.point.di
 
 import com.terning.data.datasource.MockDataSource
+import com.terning.data.datasource.ScrapDataSource
 import com.terning.data.datasourceimpl.MockDataSourceImpl
+import com.terning.data.datasourceimpl.ScrapDataSourceImpl
+import com.terning.data.repositoryimpl.ScrapRepositoryImpl
+import com.terning.domain.repository.ScrapRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -11,6 +15,10 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataSourceModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindScrapRepository(scrapDataSourceImpl: ScrapDataSourceImpl): ScrapDataSource
 
     @Binds
     @Singleton
--- app/src/main/java/com/terning/point/di/RepositoryModule.kt
+++ app/src/main/java/com/terning/point/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.terning.point.di
 
 import com.terning.data.repositoryimpl.MockRepositoryImpl
+import com.terning.data.repositoryimpl.ScrapRepositoryImpl
 import com.terning.domain.repository.MockRepository
+import com.terning.domain.repository.ScrapRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -11,6 +13,10 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun scrapRepository(scrapRepositoryImpl: ScrapRepositoryImpl): ScrapRepository
 
     @Binds
     @Singleton
