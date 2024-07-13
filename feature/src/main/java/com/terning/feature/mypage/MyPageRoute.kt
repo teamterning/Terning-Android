95881bde764c8be9f23b0bc3235ef092d8ca0ff3
LEE YOU BIN <youbeene3409@naver.com>
2024-07-13 09:56:11 +0900

    [DEL/#71] feature 모듈 Mock 관련 삭제

--- feature/src/main/java/com/terning/feature/mypage/MyPageRoute.kt
+++ feature/src/main/java/com/terning/feature/mypage/MyPageRoute.kt
@@ -1,79 +1,18 @@
 package com.terning.feature.mypage
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.flowWithLifecycle
-import com.terning.core.designsystem.component.topappbar.MyPageTopAppBar
-import com.terning.core.extension.toast
-import com.terning.core.state.UiState
-import com.terning.domain.entity.response.MockResponseModel
 
 @Composable
 fun MyPageRoute(
     viewModel: MyPageViewModel = hiltViewModel(),
 ) {
 
-    val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
 
-    val state by viewModel.state.collectAsStateWithLifecycle(lifecycleOwner = lifecycleOwner)
-
-    LaunchedEffect(key1 = true) {
-        viewModel.getFriendsInfo(2)
-    }
-
-    LaunchedEffect(viewModel.sideEffect, lifecycleOwner) {
-        viewModel.sideEffect.flowWithLifecycle(lifecycle = lifecycleOwner.lifecycle)
-            .collect { sideEffect ->
-                when (sideEffect) {
-                    is MockSideEffect.Toast -> context.toast(sideEffect.message)
-                }
-            }
-    }
-
-    when (state.followers) {
-        is UiState.Empty -> {}
-        is UiState.Loading -> {}
-        is UiState.Failure -> {}
-        is UiState.Success -> {
-            MyPageScreen(mockList = (state.followers as UiState.Success<List<MockResponseModel>>).data)
-        }
-    }
 }
 
 @Composable
 fun MyPageScreen(
-    mockList: List<MockResponseModel>,
 ) {
-    Scaffold(
-        modifier = Modifier,
-        topBar = {
-            MyPageTopAppBar(modifier = Modifier)
-        }
-    ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            items(mockList) { friend ->
-                MockItem(
-                    name = friend.firstName,
-                    profileImage = friend.avatar,
-                    email = friend.email
-                )
-            }
-        }
-    }
+
 }
--- feature/src/main/java/com/terning/feature/mypage/MyPageViewModel.kt
+++ feature/src/main/java/com/terning/feature/mypage/MyPageViewModel.kt
@@ -1,48 +1,12 @@
 package com.terning.feature.mypage
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.terning.core.state.UiState
-import com.terning.domain.entity.response.MockResponseModel
 import com.terning.domain.repository.MockRepository
-import com.terning.feature.R
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(private val repository: MockRepository) : ViewModel() {
 
-    private val _state: MutableStateFlow<MockState> =
-        MutableStateFlow(MockState())
-    val state: StateFlow<MockState> get() = _state.asStateFlow()
 
-    private val _sideEffect: MutableSharedFlow<MockSideEffect> = MutableSharedFlow()
-    val sideEffect: SharedFlow<MockSideEffect> get() = _sideEffect
-
-    fun getFriendsInfo(page: Int) {
-        viewModelScope.launch {
-            repository.getMockList(
-                page
-            ).onSuccess { response ->
-                val mockDataList = response.map { entity ->
-                    MockResponseModel(
-                        avatar = entity.avatar,
-                        email = entity.email,
-                        firstName = entity.firstName,
-                        lastName = entity.lastName
-                    )
-                }
-                _state.value = _state.value.copy(followers = UiState.Success(mockDataList))
-                _sideEffect.emit(MockSideEffect.Toast(R.string.server_success))
-            }.onFailure {
-                _sideEffect.emit(MockSideEffect.Toast(R.string.server_failure))
-            }
-        }
-    }
 }
