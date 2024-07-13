b9c18fa855c177712633e755b1201a64986e83b3
LEE YOU BIN <youbeene3409@naver.com>
2024-07-13 21:33:03 +0900

    [UI/#71] MyPageTopAppBar 구현

--- feature/src/main/java/com/terning/feature/mypage/MyPageRoute.kt
+++ feature/src/main/java/com/terning/feature/mypage/MyPageRoute.kt
@@ -1,11 +1,16 @@
 package com.terning.feature.mypage
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.terning.core.designsystem.component.topappbar.MyPageTopAppBar
 
 @Composable
 fun MyPageRoute(
-    viewModel: MyPageViewModel = hiltViewModel(),
+    myPageViewModel: MyPageViewModel = hiltViewModel(),
 ) {
 
     MyPageScreen()
@@ -14,5 +19,18 @@ fun MyPageRoute(
 @Composable
 fun MyPageScreen(
 ) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            MyPageTopAppBar(modifier = Modifier)
+        }
+    ) { paddingValues ->
 
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+
+        }
+    }
 }
+
