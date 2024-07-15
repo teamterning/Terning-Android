b5bb2cdebdc5ce4a83de5a4539f54f1662ec8f1a
arinming <jyandysys1@naver.com>
2024-07-16 01:53:51 +0900

    [FEAT/#79] 이미지 매핑

--- data/src/main/java/com/terning/data/datasourceimpl/SearchViewsDataSourceImpl.kt
+++ data/src/main/java/com/terning/data/datasourceimpl/SearchViewsDataSourceImpl.kt
@@ -16,12 +16,12 @@ class SearchViewsDataSourceImpl @Inject constructor(
                 accountments = listOf(
                     SearchViewsResponseDto.SearchViewsData(
                         internshipAnnouncementId = 23L,
-                        companyImage = "https://www.google.co.kr/url?sa=i&url=https%3A%2F%2Fm.blog.naver.com%2F41minit%2F222640892626&psig=AOvVaw37hxkUffP2inv54ayG_I3K&ust=1721147168119000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMjD_f66qYcDFQAAAAAdAAAAABAE",
+                        companyImage = "https://image.dongascience.com/Photo/2019/09/d2468576cecf1313437de5a883bfa2ed.jpg",
                         title = "[유한킴벌리]그린캠프 w. 대학생 숲 활동가 모집"
                     ),
                     SearchViewsResponseDto.SearchViewsData(
                         internshipAnnouncementId = 3L,
-                        companyImage = "https://www.google.co.kr/url?sa=i&url=https%3A%2F%2Fm.blog.naver.com%2F41minit%2F222640892626&psig=AOvVaw37hxkUffP2inv54ayG_I3K&ust=1721147168119000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMjD_f66qYcDFQAAAAAdAAAAABAE",
+                        companyImage = "https://https://image.dongascience.com/Photo/2019/09/d2468576cecf1313437de5a883bfa2ed.jpg",
                         title = "[Someone] 콘텐츠 마케터 대학생 인턴 채용"
                     )
                 )
--- feature/src/main/java/com/terning/feature/search/search/component/SearchIntern.kt
+++ feature/src/main/java/com/terning/feature/search/search/component/SearchIntern.kt
@@ -3,15 +3,17 @@ package com.terning.feature.search.search.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -19,9 +21,9 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.terning.core.designsystem.theme.Black
-import com.terning.core.designsystem.theme.Grey400
 import com.terning.core.designsystem.theme.TerningTheme
 import com.terning.core.designsystem.theme.White
+import com.terning.core.extension.customShadow
 import com.terning.domain.entity.response.SearchViewsResponseModel
 import com.terning.feature.R
 
@@ -36,12 +38,7 @@ fun SearchIntern(
         ),
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .shadow(
-                elevation = 4.dp,
-                // TODO 효빈 그림자 PR 풀 받아서 바꾸기
-                spotColor = Color(0x26DDDDDD),
-                ambientColor = Color(0x26DDDDDD)
-            )
+            .width(140.dp)
             .background(
                 color = White,
                 shape = RoundedCornerShape(size = 5.dp)
@@ -54,15 +51,22 @@ fun SearchIntern(
                 .crossfade(true)
                 .build(),
             contentDescription = stringResource(id = R.string.search_image),
+            contentScale = ContentScale.Crop,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .background(Grey400),
-
-            )
+                .height(76.dp)
+                .wrapContentSize()
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 5.dp,
+                        topEnd = 5.dp
+                    )
+                )
+        )
         Text(
-            text = searchViews.title,
-            modifier = Modifier.padding(horizontal = 8.dp),
+            text = "${searchViews.title} ${searchViews.title}${searchViews.title}${searchViews.title}",
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .wrapContentSize(),
             style = TerningTheme.typography.body6,
             color = Black,
             overflow = TextOverflow.Ellipsis,
