7d148ab7b3447e021d8b24ef5ae3fdbd544c9d32
LEE YOU BIN <youbeene3409@naver.com>
2024-07-17 14:17:58 +0900

    [FEAT/#96] 의존성 추가

--- app/src/main/java/com/terning/point/di/DataSourceModule.kt
+++ app/src/main/java/com/terning/point/di/DataSourceModule.kt
@@ -2,8 +2,10 @@ package com.terning.point.di
 
 import com.terning.data.datasource.AuthDataSource
 import com.terning.data.datasource.SearchDataSource
+import com.terning.data.datasource.TokenReissueDataSource
 import com.terning.data.datasourceimpl.AuthDataSourceImpl
 import com.terning.data.datasourceimpl.SearchDataSourceImpl
+import com.terning.data.datasourceimpl.TokenReissueDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -22,4 +24,8 @@ abstract class DataSourceModule {
     @Singleton
     abstract fun bindSearchViewsDataSource(searchViewsDataSourceImpl: SearchDataSourceImpl):
             SearchDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindTokenReissueDataSource(tokenReissueDataSourceImpl: TokenReissueDataSourceImpl): TokenReissueDataSource
 }
