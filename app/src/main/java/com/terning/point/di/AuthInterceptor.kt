6c65c85a1e803e9b9e88c0d40c6479c285cf46ce
LEE YOU BIN <youbeene3409@naver.com>
2024-07-17 13:28:27 +0900

    [FEAT/#96] RetrofitModule

--- app/src/main/java/com/terning/point/di/AuthInterceptor.kt
+++ app/src/main/java/com/terning/point/di/AuthInterceptor.kt
@@ -1,7 +1,21 @@
 package com.terning.point.di
 
+import android.content.Context
+import com.terning.data.local.TerningDataStore
+import com.terning.domain.repository.TokenReissueRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.Interceptor
+import okhttp3.Response
 import javax.inject.Inject
 
-class AuthInterceptor @Inject constructor() {
+class AuthInterceptor @Inject constructor(
+    private val tokenReissueRepository : TokenReissueRepository,
+    private val terningDataStore  : TerningDataStore,
+    @ApplicationContext private val context : Context
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        TODO("Not yet implemented")
+    }
 
 }
