339856d454be014ea40f8606d9eec4b2773f00a6
boiledegg <tjrwns2163@naver.com>
2024-07-18 16:35:19 +0900

    [UI/#55] 공용 다이얼로그 크기 고정

--- core/src/main/java/com/terning/core/designsystem/component/dialog/TerningBasicDialog.kt
+++ core/src/main/java/com/terning/core/designsystem/component/dialog/TerningBasicDialog.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -46,9 +47,7 @@ fun TerningBasicDialog(
     ) {
         Box(
             modifier = Modifier
-                .aspectRatio(300f/421f)
-                .fillMaxWidth()
-                .padding(30.dp)
+                .size(width = 300.dp, height = 412.dp)
                 .background(
                     color = White,
                     shape = RoundedCornerShape(20.dp)
