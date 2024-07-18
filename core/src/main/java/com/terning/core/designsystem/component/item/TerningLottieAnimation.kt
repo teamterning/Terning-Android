be2b3017d508dd3dd937afeb06d1ea84df7a6fcb
boiledegg <tjrwns2163@naver.com>
2024-07-19 05:04:47 +0900

    [UI/#124] 마이페이지 이미지 추가

--- core/src/main/java/com/terning/core/designsystem/component/item/TerningLottieAnimation.kt
+++ core/src/main/java/com/terning/core/designsystem/component/item/TerningLottieAnimation.kt
@@ -1,15 +1,10 @@
 package com.terning.core.designsystem.component.item
 
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 
 @Composable
