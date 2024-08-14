fb98c6dbd266573ed6e9b6eabab62b35abe864bf
LEE YOU BIN <youbeene3409@naver.com>
2024-08-14 22:22:59 +0900

    [ADD/#175] change Dokka Version

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -41,6 +41,12 @@ android {
     }
 }
 
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-base:1.6.10")
+    }
+}
+
 tasks.dokkaHtml.configure {
     dokkaSourceSets {
         named("main") {
