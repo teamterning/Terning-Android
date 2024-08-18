8cc7979bb1770974731fa5af66dda0409cd3156d
LEE YOU BIN <youbeene3409@naver.com>
2024-08-18 21:08:45 +0900

    [FIX/#180] 바텀시트 주석 제거

--- core/src/main/java/com/terning/core/designsystem/component/bottomsheet/SignUpBottomSheet.kt
+++ core/src/main/java/com/terning/core/designsystem/component/bottomsheet/SignUpBottomSheet.kt
@@ -33,14 +33,6 @@ import com.terning.core.designsystem.theme.TerningTheme
 import com.terning.core.extension.noRippleClickable
 import kotlinx.coroutines.launch
 
-/**
- * 회원가입을 할 때 프로필 이미지를 선택할 수 있는 바텀시트입니다.
- *
- * @param modifier 바텀시트에 적용할 Modifier입니다.
- * @param onDismiss 바텀시트가 닫힐 때 호출되는 콜백 함수입니다.
- * @param onSaveClick 저장하기 버튼 클릭 시, 호출되는 콜백 함수입니다.
- * @param initialSelectedOption 초기에 선택된 이미지를 나타내는 인덱스 값입니다.
- */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpBottomSheet(
--- core/src/main/java/com/terning/core/designsystem/component/bottomsheet/TerningBasicBottomSheet.kt
+++ core/src/main/java/com/terning/core/designsystem/component/bottomsheet/TerningBasicBottomSheet.kt
@@ -7,13 +7,6 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import com.terning.core.designsystem.theme.White
 
-/**
- * 기본 바텀시트 함수입니다.
- *
- * @param content 바텀시트에 표시할 내용을 담은 Composable 함수입니다.
- * @param onDismissRequest 바텀시트가 닫히는 요청 시 호출되는 콜백 함수입니다.
- * @param sheetState 바텀시트의 상태를 관리하는 SheetState 객체입니다.
- */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TerningBasicBottomSheet(
