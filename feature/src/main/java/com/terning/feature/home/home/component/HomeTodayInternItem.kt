3c3cdd58bb0244897ce4d0d5b36289eb02260f8a
Hyobeen-Park <amanda0109@naver.com>
2024-08-29 05:21:20 +0900

    [UI/#204] 홈 뷰 곧 마감되는 공고 아이템 수정

--- feature/src/main/java/com/terning/feature/home/home/component/HomeTodayInternItem.kt
+++ feature/src/main/java/com/terning/feature/home/home/component/HomeTodayInternItem.kt
@@ -1,53 +1,126 @@
 package com.terning.feature.home.home.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.terning.core.designsystem.component.item.ScrapBox
 import com.terning.core.designsystem.theme.Black
 import com.terning.core.designsystem.theme.Grey150
+import com.terning.core.designsystem.theme.Grey500
+import com.terning.core.designsystem.theme.TerningMain
+import com.terning.core.designsystem.theme.TerningSub3
 import com.terning.core.designsystem.theme.TerningTheme
+import com.terning.domain.entity.home.HomeTodayIntern
+import com.terning.feature.R
 
 @Composable
 fun HomeTodayInternItem(
-    title: String,
-    scrapColor: Color,
+    homeTodayIntern: HomeTodayIntern,
     modifier: Modifier = Modifier
 ) {
     ScrapBox(
-        modifier = modifier
-            .height(116.dp)
-            .width(140.dp),
+        modifier = Modifier
+            .width(246.dp),
         cornerRadius = 5.dp,
-        scrapColor = scrapColor,
+        scrapColor = Color(android.graphics.Color.parseColor(homeTodayIntern.color)),
         borderWidth = 1.dp,
         borderColor = Grey150,
         content = {
             Column(
                 modifier = modifier
                     .fillMaxHeight(),
-                verticalArrangement = Arrangement.Bottom
+                verticalArrangement = Arrangement.SpaceBetween,
             ) {
                 Text(
-                    text = title,
+                    text = homeTodayIntern.title,
                     modifier = modifier
                         .padding(
-                            start = 8.dp,
-                            end = 9.dp,
-                            bottom = 8.dp
+                            start = 12.dp,
+                            end = 12.dp,
+                            top = 16.dp
                         ),
                     style = TerningTheme.typography.button3,
                     color = Black,
                     maxLines = 3,
+                    minLines = 3,
                 )
+
+                Row(
+                    modifier = Modifier
+                        .padding(
+                            start = 12.dp,
+                            end = 12.dp,
+                            bottom = 12.dp,
+                            top = 8.dp
+                        )
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(homeTodayIntern.companyImage)
+                            .build(),
+                        contentDescription = stringResource(id = R.string.home_company_image),
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .height(32.dp)
+                            .aspectRatio(1f)
+                            .border(BorderStroke(1.dp, Grey150), CircleShape)
+                            .clip(CircleShape)
+                    )
+
+                    Text(
+                        text = "유한킴벌리",
+                        style = TerningTheme.typography.button5,
+                        color = Grey500,
+                        maxLines = 1,
+                        modifier = Modifier
+                            .padding(start = 6.dp)
+                            .weight(1f)
+                    )
+
+                    Row(
+                        modifier = Modifier
+                            .size(width = 40.dp, height = 20.dp)
+                            .background(
+                                color = TerningSub3,
+                                shape = RoundedCornerShape(size = 5.dp)
+                            ),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = homeTodayIntern.dDay,
+                            style = TerningTheme.typography.body4,
+                            color = TerningMain,
+                        )
+                    }
+                }
             }
         }
     )
