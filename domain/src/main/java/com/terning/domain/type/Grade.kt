edc307f19d977076ca8be2efadd91ccd07da4b3c
Hyobeen-Park <amanda0109@naver.com>
2024-08-31 04:29:14 +0900

    [UI/#204] domain/type에 enum class 추가

---
+++ domain/src/main/java/com/terning/domain/type/Grade.kt
@@ -0,0 +1,8 @@
+package com.terning.domain.type
+
+enum class Grade(val grade: String) {
+    FRESHMAN("freshman"),
+    SOPHOMORE("sophomore"),
+    JUNIOR("junior"),
+    SENIOR("senior"),
+}
---
+++ domain/src/main/java/com/terning/domain/type/WorkingPeriod.kt
@@ -0,0 +1,7 @@
+package com.terning.domain.type
+
+enum class WorkingPeriod {
+    FIRST,
+    SECOND,
+    THIRD,
+}
