cc8b5d59bcdd9318830bc25f77c7fbff8b9bd462
arinming <jyandysys1@naver.com>
2024-09-06 20:43:10 +0900

    [FEAT/#220] InternInfoMapper 생성

--- data/src/main/java/com/terning/data/mapper/intern/InternInfoMapper.kt
+++ data/src/main/java/com/terning/data/mapper/intern/InternInfoMapper.kt
@@ -1,4 +1,22 @@
 package com.terning.data.mapper.intern
 
-class InternInfoMapper {
-}
+import com.terning.domain.entity.intern.InternInfo
+
+fun InternInfo.toInternInfo(): InternInfo =
+    InternInfo(
+        dDay = dDay,
+        title = title,
+        deadline = deadline,
+        workingPeriod = workingPeriod,
+        startDate = startDate,
+        scrapCount = scrapCount,
+        viewCount = viewCount,
+        company = company,
+        companyCategory = companyCategory,
+        companyImage = companyImage,
+        qualification = qualification,
+        jobType = jobType,
+        detail = detail,
+        url = url,
+        scrapId = scrapId
+    )
