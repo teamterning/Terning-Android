b3f3efbac8ad5cd6d3cef5e115c08067518e791c
Hyobeen-Park <amanda0109@naver.com>
2024-09-08 16:55:25 +0900

    [MOD/#204] enum 함수 추가

--- core/src/main/java/com/terning/core/type/Grade.kt
+++ core/src/main/java/com/terning/core/type/Grade.kt
@@ -4,11 +4,21 @@ import androidx.annotation.StringRes
 import com.terning.core.R
 
 enum class Grade(
-    val grade: String,
-    @StringRes val text: Int,
+    val stringValue: String,
+    @StringRes val stringResId: Int,
 ) {
     FRESHMAN("freshman", R.string.change_filter_grade_1),
     SOPHOMORE("sophomore", R.string.change_filter_grade_2),
     JUNIOR("junior", R.string.change_filter_grade_3),
-    SENIOR("senior", R.string.change_filter_grade_4),
+    SENIOR("senior", R.string.change_filter_grade_4);
+
+    companion object {
+        fun fromString(value: String?): Grade = when (value) {
+            "freshman" -> FRESHMAN
+            "sophomore" -> SOPHOMORE
+            "junior" -> JUNIOR
+            "senior" -> SENIOR
+            else -> FRESHMAN
+        }
+    }
 }
--- core/src/main/java/com/terning/core/type/WorkingPeriod.kt
+++ core/src/main/java/com/terning/core/type/WorkingPeriod.kt
@@ -4,10 +4,19 @@ import androidx.annotation.StringRes
 import com.terning.core.R
 
 enum class WorkingPeriod(
-    val workingPeriod: String,
-    @StringRes val text: Int,
+    val stringValue: String,
+    @StringRes val stringResId: Int,
 ) {
     SHORT("short", R.string.change_filter_period_1),
     MIDDLE("middle", R.string.change_filter_period_2),
-    LONG("long", R.string.change_filter_period_3),
+    LONG("long", R.string.change_filter_period_3);
+
+    companion object {
+        fun fromString(value: String?): WorkingPeriod = when (value) {
+            "short" -> SHORT
+            "middle" -> MIDDLE
+            "long" -> LONG
+            else -> SHORT
+        }
+    }
 }
