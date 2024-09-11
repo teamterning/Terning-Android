479a42bbe1d25ef694d57757faff7212d909169c
LEE YOU BIN <youbeene3409@naver.com>
2024-09-11 15:57:23 +0900

    [FEAT/#238] URL 수정

--- feature/src/main/java/com/terning/feature/mypage/mypage/util/MyPageDefaults.kt
+++ feature/src/main/java/com/terning/feature/mypage/mypage/util/MyPageDefaults.kt
@@ -3,7 +3,7 @@ package com.terning.feature.mypage.mypage.util
 object MyPageDefaults {
     const val VERSION = "1.1.0"
     const val NOTICE_URL =
-        "https://abundant-quiver-13f.notion.site/AOS-49b3cc2390ee4dc389e25a5097736944?pvs=4"
+        "https://abundant-quiver-13f.notion.site/Android-49b3cc2390ee4dc389e25a5097736944"
     const val OPINION_URL = "https://forms.gle/AaLpVptfg6cATYWa7"
     const val SERVICE_URL =
         "https://abundant-quiver-13f.notion.site/69109213e7db4873be6b9600f2f5163a?pvs=4"
