a3399ec5b65501e9804d5bf1b9dca81fc5ee2caf
LEE YOU BIN <youbeene3409@naver.com>
2024-09-12 03:38:29 +0900

    [CHORE/#233] 코드 수정

--- feature/src/main/java/com/terning/feature/filtering/filteringone/navigation/FilteringOneNavigation.kt
+++ feature/src/main/java/com/terning/feature/filtering/filteringone/navigation/FilteringOneNavigation.kt
@@ -8,8 +8,8 @@ import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
 import com.terning.core.navigation.Route
-import com.terning.feature.filtering.filteringone.FilteringOneRoute
 import kotlinx.serialization.Serializable
+import com.terning.feature.filtering.filteringone.FilteringOneRoute
 
 fun NavController.navigateFilteringOne(
     name: String,
