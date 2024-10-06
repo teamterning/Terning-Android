3bac220372c92a6fd5f890047d3adb608be24f89
Hyobeen-Park <amanda0109@naver.com>
2024-10-07 08:56:38 +0900

    [FIX/#279] 곧 마감 공고 카드 그림자 연하게 수정

--- feature/src/main/java/com/terning/feature/home/component/HomeUpcomingEmptyFilter.kt
+++ feature/src/main/java/com/terning/feature/home/component/HomeUpcomingEmptyFilter.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.terning.core.designsystem.component.image.TerningImage
-import com.terning.core.designsystem.theme.Grey200
+import com.terning.core.designsystem.theme.Grey150
 import com.terning.core.designsystem.theme.Grey500
 import com.terning.core.designsystem.theme.TerningTheme
 import com.terning.core.designsystem.theme.White
@@ -31,9 +31,9 @@ fun HomeUpcomingEmptyFilter(
             .fillMaxWidth()
             .padding(horizontal = 24.dp)
             .customShadow(
-                color = Grey200,
+                color = Grey150,
                 shadowRadius = 5.dp,
-                shadowWidth = 2.dp
+                shadowWidth = 1.dp
             )
             .background(
                 color = White,
