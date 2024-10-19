04ff873d7add7f34adddc7027ff16a76111ba250
LEE YOU BIN <youbeene3409@naver.com>
2024-10-19 14:37:23 +0900

    [FEAT/#288] put amplitude tracker in activity

--- feature/src/main/java/com/terning/feature/main/MainActivity.kt
+++ feature/src/main/java/com/terning/feature/main/MainActivity.kt
@@ -4,18 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.CompositionLocalProvider
+import com.terning.core.analytics.AmplitudeTracker
+import com.terning.core.analytics.LocalTracker
 import com.terning.core.designsystem.theme.TerningPointTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var tracker: AmplitudeTracker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             val navigator: MainNavigator = rememberMainNavigator()
             TerningPointTheme {
-                MainScreen(navigator)
+                CompositionLocalProvider(LocalTracker provides tracker) {
+                    MainScreen(navigator)
+                }
             }
         }
     }
