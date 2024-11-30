1fd179a3d994ec1e1fd3cb55a8fe2d59d46d4a69
LEE YOU BIN <youbeene3409@naver.com>
2024-12-01 00:57:49 +0900

    [FEAT/#302] domain:auth 모듈 정리

--- domain/auth/src/main/java/com/terning/domain/auth/repository/AuthRepository.kt
+++ domain/auth/src/main/java/com/terning/domain/auth/repository/AuthRepository.kt
@@ -1,9 +1,9 @@
 package com.terning.domain.auth.repository
 
-import com.terning.domain.entity.onboarding.SignInRequest
-import com.terning.domain.entity.onboarding.SignUpRequest
-import com.terning.domain.entity.onboarding.SignInResponse
-import com.terning.domain.entity.onboarding.SignUpResponse
+import com.terning.domain.auth.entity.SignInRequest
+import com.terning.domain.auth.entity.SignInResponse
+import com.terning.domain.auth.entity.SignUpRequest
+import com.terning.domain.auth.entity.SignUpResponse
 
 interface AuthRepository {
 
