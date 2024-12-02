aa5e93c4cdf2c771cdf1c676ec79062dc539cbf0
LEE YOU BIN <youbeene3409@naver.com>
2024-12-03 02:57:08 +0900

    [FEAT/#302] 프로가드, 컨슈머 삭제

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -3,10 +3,20 @@ import com.terning.build_logic.setNamespace
 plugins {
     alias(libs.plugins.terning.library)
     alias(libs.plugins.terning.compose)
+    alias(libs.plugins.dokka)
 }
 
 android {
     setNamespace("core.designsystem")
+}
+
+tasks.dokkaHtml.configure {
+    dokkaSourceSets {
+        named("main") {
+            includes.from("docs.md")
+            noAndroidSdkLink.set(false)
+        }
+    }
 }
 
 dependencies {
