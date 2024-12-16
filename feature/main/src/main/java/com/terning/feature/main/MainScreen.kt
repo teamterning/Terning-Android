8aed3efbedc8583632693f2e9c48a57aef756935
LEE YOU BIN <youbeene3409@naver.com>
2024-12-16 20:26:23 +0900

    [CHORE/#309] import문 수정

--- feature/main/src/main/java/com/terning/feature/main/MainScreen.kt
+++ feature/main/src/main/java/com/terning/feature/main/MainScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
@@ -294,7 +295,7 @@ private fun MainBottomBar(
                             fontSize = 9.sp
                         )
                     },
-                    colors = androidx.compose.material3.NavigationBarItemDefaults
+                    colors = NavigationBarItemDefaults
                         .colors(
                             selectedIconColor = TerningMain,
                             selectedTextColor = TerningMain,
