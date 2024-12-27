f392528b67b58fc1a743f9a112e6bc7eb4500bb4
Hyobeen-Park <amanda0109@naver.com>
2024-12-27 15:46:07 +0900

    [CHORE/#313] 직무 카테고리 이미지 수정

--- core/designsystem/src/main/java/com/terning/core/designsystem/type/JobType.kt
+++ core/designsystem/src/main/java/com/terning/core/designsystem/type/JobType.kt
@@ -15,7 +15,7 @@ enum class JobType(
     ADMIN("admin", R.drawable.ic_job_accounting_24, R.string.job_type_admin),
     DESIGN("design", R.drawable.ic_job_design_24, R.string.job_type_design),
     IT("it", R.drawable.ic_job_it_24, R.string.job_type_it),
-    RESEARCH("research", R.drawable.ic_bookmark_filled, R.string.job_type_research),
+    RESEARCH("research", R.drawable.ic_job_research_24, R.string.job_type_research),
     ETC("etc", R.drawable.ic_job_extra_24, R.string.job_type_etc),
     TOTAL("total", R.drawable.ic_job_all_24, R.string.job_type_total), ;
 
