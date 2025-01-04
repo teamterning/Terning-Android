7342eff68d47213db5fbf1eaf95fb403390c2894
arinming <jyandysys1@naver.com>
2025-01-04 16:23:57 +0900

    [DEL/#306] 사용하지 않는 코드 삭제

--- feature/search/src/main/java/com/terning/feature/search/searchprocess/navigation/SearchProcessNavigation.kt
+++ feature/search/src/main/java/com/terning/feature/search/searchprocess/navigation/SearchProcessNavigation.kt
@@ -1,8 +1,6 @@
 package com.terning.feature.search.searchprocess.navigation
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
