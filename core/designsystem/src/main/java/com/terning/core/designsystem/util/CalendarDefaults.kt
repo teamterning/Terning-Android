9cf3f8cf48da8d559626f98e2868b23e0e8d83ea
LEE YOU BIN <youbeene3409@naver.com>
2025-01-07 19:54:47 +0900

    [FIX/#322] 데이트피커 범위 축소

--- core/designsystem/src/main/java/com/terning/core/designsystem/util/CalendarDefaults.kt
+++ core/designsystem/src/main/java/com/terning/core/designsystem/util/CalendarDefaults.kt
@@ -1,8 +1,14 @@
 package com.terning.core.designsystem.util
 
+import com.terning.core.designsystem.extension.currentMonth
+import com.terning.core.designsystem.extension.currentYear
+import java.util.Calendar
+
 object CalendarDefaults {
     const val START_YEAR = 2010
-    const val END_YEAR = 2030
+    val END_YEAR =
+        if (Calendar.getInstance().currentMonth == 10) Calendar.getInstance().currentYear + 1
+        else Calendar.getInstance().currentYear
     const val START_MONTH = 1
     const val END_MONTH = 12
 }
