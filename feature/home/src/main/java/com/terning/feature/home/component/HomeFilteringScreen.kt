c865b8a8a08c86dd1c946e1ec5c95e28583efab1
Hyobeen-Park <amanda0109@naver.com>
2025-02-26 22:41:32 +0900

    [FEAT/#343] 필터링 재설정 버튼 pressed 버전 추가

--- feature/home/src/main/java/com/terning/feature/home/component/HomeFilteringScreen.kt
+++ feature/home/src/main/java/com/terning/feature/home/component/HomeFilteringScreen.kt
@@ -1,22 +1,29 @@
 package com.terning.feature.home.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.terning.core.analytics.EventType
 import com.terning.core.analytics.LocalTracker
-import com.terning.core.designsystem.extension.noRippleClickable
 import com.terning.core.designsystem.theme.TerningMain
+import com.terning.core.designsystem.theme.TerningSub5
 import com.terning.core.designsystem.theme.TerningTheme
 import com.terning.feature.home.R
 
@@ -25,6 +32,9 @@ fun HomeFilteringScreen(
     onChangeFilterClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
     val amplitudeTracker = LocalTracker.current
 
     Row(
@@ -35,13 +45,21 @@ fun HomeFilteringScreen(
                 color = TerningMain,
                 shape = RoundedCornerShape(5.dp)
             )
-            .noRippleClickable {
-                amplitudeTracker.track(
-                    type = EventType.CLICK,
-                    name = "home_filtering"
-                )
-                onChangeFilterClick()
-            },
+            .background(
+                color = if (isPressed) TerningSub5 else Color.Transparent,
+                shape = RoundedCornerShape(5.dp)
+            )
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = {
+                    amplitudeTracker.track(
+                        type = EventType.CLICK,
+                        name = "home_filtering"
+                    )
+                    onChangeFilterClick()
+                }
+            )
     ) {
         Icon(
             painter = painterResource(id = R.drawable.ic_home_filtering_28),
