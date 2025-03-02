0e423a926f580d655e0469ba8f26766a19a81698
leeeyubin <128459613+leeeyubin@users.noreply.github.com>
2025-03-02 17:12:31 +0000

    Deploying to gh-pages from @ teamterning/Terning-Android@23024ac7309cd0834d03aa980e43001b6de71b85 🚀

--- scripts/sourceset_dependencies.js
+++ scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":core:designsystem:dokkaHtml/debug":[],":core:designsystem:dokkaHtml/generatedByKspDebugKotlin":[],":core:designsystem:dokkaHtml/generatedByKspReleaseKotlin":[],":core:designsystem:dokkaHtml/main":[],":core:designsystem:dokkaHtml/release":[]}'
+sourceset_dependencies='{":core:designsystem:dokkaHtml/debug":[],":core:designsystem:dokkaHtml/main":[],":core:designsystem:dokkaHtml/release":[]}'
