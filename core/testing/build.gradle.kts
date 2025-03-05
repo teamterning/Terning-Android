32677b52a7bee21d81f15e94d1094dcb430f764b
arinming <jyandysys1@naver.com>
2025-03-05 21:29:18 +0900

    [MOVE/#351] gradle core:test로 통합

--- core/testing/build.gradle.kts
+++ core/testing/build.gradle.kts
@@ -8,7 +8,12 @@ android {
 
 dependencies {
     api(libs.kotlinx.coroutines.test)
+    api(libs.hilt.android.testing)
+    api(libs.mockito.kotlin)
+    api(libs.coroutines.test)
 
     implementation(libs.androidx.test.rules)
     implementation(libs.hilt.android.testing)
+
+    testImplementation(libs.bundles.androidx.compose.ui.test)
 }
