74306c85260a1035a525db742f6df4de6a657c00
LEE YOU BIN <youbeene3409@naver.com>
2025-03-20 03:59:58 +0900

    [FEAT/#352] 메세지 서비스 매니페스트에 등록

--- app/src/main/java/com/terning/point/TerningMessagingService.kt
+++ app/src/main/java/com/terning/point/TerningMessagingService.kt
@@ -1,13 +1,19 @@
 package com.terning.point
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class TerningMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
+        // FCM 토큰이 갱신될 때마다 호출
         super.onNewToken(token)
+        Log.d("MyFirebaseMessagingService", "fcm token : $token")
     }
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        Log.d("MyFirebaseMessagingService", message.notification.toString())
+        Log.d("MyFirebaseMessagingService", message.data.toString())
+        Log.d("MyFirebaseMessagingService", "터닝 화이팅")
     }
 }
