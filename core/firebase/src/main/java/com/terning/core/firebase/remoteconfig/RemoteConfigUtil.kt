a3452402e90962cd37a690c8c2d20f7ae477b603
boiledegg <tjrwns2163@naver.com>
2025-03-24 02:02:19 +0900

    [del/#363] 디스패처 제거

--- core/firebase/src/main/java/com/terning/core/firebase/remoteconfig/RemoteConfigUtil.kt
+++ core/firebase/src/main/java/com/terning/core/firebase/remoteconfig/RemoteConfigUtil.kt
@@ -4,12 +4,10 @@ import com.google.firebase.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.remoteConfig
 import com.google.firebase.remoteconfig.remoteConfigSettings
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import timber.log.Timber
 import com.terning.core.firebase.remoteconfig.RemoteConfigKey.LATEST_APP_VERSION
 import com.terning.core.firebase.remoteconfig.RemoteConfigKey.MAJOR_UPDATE_BODY
 import com.terning.core.firebase.remoteconfig.RemoteConfigKey.MAJOR_UPDATE_TITLE
+import timber.log.Timber
 
 object RemoteConfigUtil {
     private const val TAG = "FirebaseRemoteConfig"
@@ -30,25 +28,24 @@ object RemoteConfigUtil {
         }
     }
 
-    suspend fun onVersionFetchCompleteListener(callback: (Map<RemoteConfigKey, String>) -> Unit) {
-        withContext(Dispatchers.IO) {
-            try {
-                val fetchTask = remoteConfig.fetchAndActivate()
-                fetchTask.addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val map = mutableMapOf(
-                            getConfigKeyToStringPair(LATEST_APP_VERSION),
-                            getConfigKeyToStringPair(MAJOR_UPDATE_TITLE),
-                            getConfigKeyToStringPair(MAJOR_UPDATE_BODY),
-                        )
-                        callback(map)
-                    }
+    fun onVersionFetchCompleteListener(callback: (Map<RemoteConfigKey, String>) -> Unit) {
+        try {
+            val fetchTask = remoteConfig.fetchAndActivate()
+            fetchTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val map = mutableMapOf(
+                        getConfigKeyToStringPair(LATEST_APP_VERSION),
+                        getConfigKeyToStringPair(MAJOR_UPDATE_TITLE),
+                        getConfigKeyToStringPair(MAJOR_UPDATE_BODY),
+                    )
+                    callback(map)
                 }
-            } catch (e: Exception) {
-                Timber.tag(TAG).e(e.printStackTrace().toString())
             }
+        } catch (e: Exception) {
+            Timber.tag(TAG).e(e.printStackTrace().toString())
         }
     }
+
 
     private fun getConfigKeyToStringPair(configKey: RemoteConfigKey): Pair<RemoteConfigKey, String> =
         configKey to remoteConfig.getString(configKey.key)
