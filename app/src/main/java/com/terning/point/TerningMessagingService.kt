3f25376f12ef6585202464062fbcada89b6900bc
LEE YOU BIN <youbeene3409@naver.com>
2025-03-24 21:40:17 +0900

    [FEAT/#352] Messaging Service 작성

--- app/src/main/java/com/terning/point/TerningMessagingService.kt
+++ app/src/main/java/com/terning/point/TerningMessagingService.kt
@@ -1,19 +1,80 @@
 package com.terning.point
 
-import android.util.Log
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.terning.core.local.TerningDataStore
+import com.terning.feature.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
 
-class TerningMessagingService : FirebaseMessagingService() {
+@AndroidEntryPoint
+class TerningMessagingService : FirebaseMessagingService() {  // todo : 파이어베이스 모듈로 옮기기 by leeeyubin
+
+    @Inject
+    lateinit var terningDataStore: TerningDataStore
+
     override fun onNewToken(token: String) {
-        // FCM 토큰이 갱신될 때마다 호출
         super.onNewToken(token)
-        Log.d("MyFirebaseMessagingService", "fcm token : $token")
+
+        Timber.tag("okhttp").d("ON NEW TOKEN")
     }
+
+    override fun handleIntent(intent: Intent?) {
+        super.handleIntent(intent)
+        if (intent?.getStringExtra("title")?.isNullOrEmpty() == true) return
+
+        val title = intent?.getStringExtra("title").orEmpty()
+        val body = intent?.getStringExtra("body").orEmpty()
+        val type = intent?.getStringExtra("type").orEmpty()
+        sendNotification(title, body, type)
+    }
+
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        Log.d("MyFirebaseMessagingService", message.notification.toString())
-        Log.d("MyFirebaseMessagingService", message.data.toString())
-        Log.d("MyFirebaseMessagingService", "터닝 화이팅")
+
+        if (message.data.isEmpty() || !terningDataStore.alarmAvailable) return
+
+        val title = message.data["title"].orEmpty()
+        val body = message.data["body"].orEmpty()
+        val type = message.data["type"].orEmpty()
+
+        sendNotification(title = title, body = body, type)
+    }
+
+    private fun sendNotification(title: String?, body: String?, type: String?) {
+        val notifyId = System.currentTimeMillis().toInt()
+        val intent =
+            MainActivity.getIntent(this, type = type).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            notifyId,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_MUTABLE,
+        )
+        val channelId: String = CHANNEL_ID
+        val notificationBuilder =
+            NotificationCompat.Builder(this, channelId).apply {
+                setSmallIcon(R.mipmap.ic_terning_launcher)
+                setContentTitle(title)
+                setContentText(body)
+                setPriority(NotificationManagerCompat.IMPORTANCE_HIGH)
+                setContentIntent(pendingIntent)
+            }
+
+        getSystemService<NotificationManager>()?.run {
+            notify(notifyId, notificationBuilder.build())
+        }
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "TERNING"
+        private const val TYPE = "TYPE"
     }
 }
--- feature/main/src/main/java/com/terning/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/terning/feature/main/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.terning.feature.main
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import com.terning.core.analytics.AmplitudeTracker
 import com.terning.core.analytics.LocalTracker
+import com.terning.core.designsystem.extension.toast
 import com.terning.core.designsystem.theme.TerningPointTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -22,11 +28,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val navigator: MainNavigator = rememberMainNavigator()
+            val intentData = intent.getStringExtra(EXTRA_TYPE)
+            val isFromNotification = intentData != null
+            val context = LocalContext.current
+            LaunchedEffect(true) {
+                if (isFromNotification) {
+                    Toast.makeText(context, "$intentData", Toast.LENGTH_SHORT).show()
+                }
+            }
+
             TerningPointTheme {
                 CompositionLocalProvider(LocalTracker provides tracker) {
                     MainScreen(navigator)
                 }
             }
+        }
+    }
+
+    companion object {
+        private const val EXTRA_TYPE = "EXTRA_DEFAULT"
+
+        @JvmStatic
+        fun getIntent(
+            context: Context,
+            type: String? = null,
+        ) = Intent(context, MainActivity::class.java).apply {
+            putExtra(EXTRA_TYPE, type)
         }
     }
 }
