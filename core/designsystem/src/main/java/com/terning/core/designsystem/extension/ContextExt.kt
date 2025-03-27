4a69e92909cd3b65136745021e86e8bb632c6c8e
boiledegg <tjrwns2163@naver.com>
2025-03-28 01:43:58 +0900

    [FEAT/#368] Context 확장함수 구현
    - 패키지 버전이름 불러오기
    - 터닝 플레이스토어 이동하기

--- core/designsystem/src/main/java/com/terning/core/designsystem/extension/ContextExt.kt
+++ core/designsystem/src/main/java/com/terning/core/designsystem/extension/ContextExt.kt
@@ -1,8 +1,11 @@
 package com.terning.core.designsystem.extension
 
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.core.net.toUri
+import timber.log.Timber
 
 fun Context.toast(@StringRes message: Int) {
     Toast.makeText(this, getString(message), Toast.LENGTH_SHORT).show()
@@ -14,4 +17,23 @@ fun Context.stringToast(message: String) {
 
 fun Context.longToast(@StringRes message: Int) {
     Toast.makeText(this, getString(message), Toast.LENGTH_SHORT).show()
+}
+
+fun Context.getVersionName(): String? = runCatching {
+    packageManager.getPackageInfo(packageName, 0)
+}.fold(
+    onSuccess = { it.versionName },
+    onFailure = {
+        Timber.e(it)
+        null
+    }
+)
+
+fun Context.launchPlayStore() = runCatching {
+    val terningPlayStoreUri = "https://play.google.com/store/apps/details?id=com.terning.point".toUri()
+    Intent(Intent.ACTION_VIEW).apply { data = terningPlayStoreUri }
+}.onSuccess { playStoreIntent ->
+    startActivity(playStoreIntent)
+}.onFailure {
+    Timber.e(it)
 }
