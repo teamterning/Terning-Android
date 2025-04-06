82abc22e1f974ffcab75de2c8b31f7fa160c382f
LEE YOU BIN <youbeene3409@naver.com>
2025-04-07 00:41:36 +0900

    [DEL/#372] MyPageNavigation 예시 삭제

--- feature/mypage/src/main/java/com/terning/feature/mypage/mypage/navigation/MyPageNavigation.kt
+++ feature/mypage/src/main/java/com/terning/feature/mypage/mypage/navigation/MyPageNavigation.kt
@@ -1,13 +1,11 @@
 package com.terning.feature.mypage.mypage.navigation
 
-import android.content.Intent
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import androidx.navigation.navDeepLink
 import com.terning.core.navigation.MainTabRoute
 import com.terning.feature.mypage.mypage.MyPageRoute
 import com.terning.feature.mypage.profileedit.navigation.navigateProfileEdit
@@ -25,15 +23,7 @@ fun NavGraphBuilder.myPageNavGraph(
     navHostController: NavHostController,
     restartApp: () -> Unit
 ) {
-    composable<MyPage>(
-        deepLinks = listOf(
-            navDeepLink {
-                uriPattern = "terning://mypage"
-                action = Intent.ACTION_VIEW
-                mimeType = "*/*"
-            }
-        )
-    ) {
+    composable<MyPage> {
         MyPageRoute(
             paddingValues = paddingValues,
             navigateToProfileEdit = { name, profileImage, authType ->
