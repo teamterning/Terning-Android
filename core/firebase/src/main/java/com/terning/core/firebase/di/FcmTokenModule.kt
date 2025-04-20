7e6e7d38fa4692b08b226d5d3781f4bd9b1ac86c
LEE YOU BIN <youbeene3409@naver.com>
2025-04-20 17:58:50 +0900

    [FEAT/#378] 네이밍 수정

--- core/firebase/src/main/java/com/terning/core/firebase/di/FcmTokenModule.kt
+++ core/firebase/src/main/java/com/terning/core/firebase/di/FcmTokenModule.kt
@@ -10,8 +10,8 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object FcmFirebaseModule {
+object FcmTokenModule {
     @Provides
     @Singleton
-    fun provideFcmFirebaseProviderImpl(): FcmTokenProvider = FcmTokenProviderImpl()
+    fun provideFcmTokenProviderImpl(): FcmTokenProvider = FcmTokenProviderImpl()
 }
