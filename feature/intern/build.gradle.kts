b279d273520370fd73a5adfe2da3e7d7e29a5dd1
arinming <jyandysys1@naver.com>
2025-04-23 09:15:11 +0900

    [DELETE/#364] 필요없는 gradle 삭제

--- feature/intern/build.gradle.kts
+++ feature/intern/build.gradle.kts
@@ -11,7 +11,6 @@ android {
 dependencies {
     // domain
     implementation(projects.domain.intern)
-    implementation(libs.kakao.share)
 
     // feature
     implementation(projects.feature.dialog)
