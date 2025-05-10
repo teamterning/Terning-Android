b3f828a85ce2185f5a9ac6dc514ad9f118345203
LEE YOU BIN <youbeene3409@naver.com>
2025-05-11 03:05:40 +0900

    [FEAT/#380] 파라미터 base -> host 수정

--- core/designsystem/src/main/java/com/terning/core/designsystem/util/DeeplinkDefaults.kt
+++ core/designsystem/src/main/java/com/terning/core/designsystem/util/DeeplinkDefaults.kt
@@ -6,10 +6,14 @@ object DeeplinkDefaults {
     const val REDIRECT: String = "redirect"
     const val INTERN_ID: String = "internId"
 
-    fun build(base: String, redirect: String? = null, internId: String? = null): String {
+    fun build(
+        host: String,
+        redirect: String? = null,
+        internId: String? = null
+    ): String {
         val uriBuilder = Uri.Builder()
             .scheme("terning")
-            .authority(base)
+            .authority(host)
 
         redirect?.let { uriBuilder.appendQueryParameter(REDIRECT, it) }
         internId?.let { uriBuilder.appendQueryParameter(INTERN_ID, it) }
