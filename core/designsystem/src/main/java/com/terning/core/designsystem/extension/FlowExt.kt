031018589058f1e73c18a8fec8853e62829d148c
LEE YOU BIN <youbeene3409@naver.com>
2025-05-17 11:36:57 +0900

    [FEAT/#396] groupBy 확장함수 작성

---
+++ core/designsystem/src/main/java/com/terning/core/designsystem/extension/FlowExt.kt
@@ -0,0 +1,23 @@
+package com.terning.core.designsystem.extension
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.flow
+
+fun <T, K> Flow<T>.groupBy(getKey: (T) -> K): Flow<Pair<K, Flow<T>>> = flow {
+    val storage = mutableMapOf<K, SendChannel<T>>()
+
+    collect { t ->
+        val key = getKey(t)
+        val channel = storage.getOrPut(key) {
+            Channel<T>(capacity = Channel.BUFFERED).also {
+                emit(key to it.consumeAsFlow())
+            }
+        }
+        channel.send(t)
+    }
+
+    storage.values.forEach { it.close() }
+}
