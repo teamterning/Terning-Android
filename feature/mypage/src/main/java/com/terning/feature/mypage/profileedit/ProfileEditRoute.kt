bdae0b5e66982f95c3c976a5a91abe20d2fe6077
LEE YOU BIN <youbeene3409@naver.com>
2025-08-27 08:34:40 +0900

    [REFACTOR/#402] onClick 리컴포지션 잡기

--- feature/mypage/src/main/java/com/terning/feature/mypage/profileedit/ProfileEditRoute.kt
+++ feature/mypage/src/main/java/com/terning/feature/mypage/profileedit/ProfileEditRoute.kt
@@ -30,7 +30,6 @@ import com.terning.core.designsystem.component.item.ProfileWithPlusButton
 import com.terning.core.designsystem.component.textfield.NameTextField
 import com.terning.core.designsystem.component.topappbar.BackButtonTopAppBar
 import com.terning.core.designsystem.extension.addFocusCleaner
-import com.terning.core.designsystem.extension.noRippleClickable
 import com.terning.core.designsystem.extension.toast
 import com.terning.core.designsystem.theme.Grey500
 import com.terning.core.designsystem.theme.TerningPointTheme
@@ -140,11 +139,10 @@ fun ProfileEditScreen(
             )
             Spacer(modifier = Modifier.height(20.dp))
             ProfileWithPlusButton(
-                modifier = Modifier
-                    .noRippleClickable {
-                        onProfileEditClick(true)
-                    }
-                    .align(Alignment.CenterHorizontally),
+                onClick = {
+                    onProfileEditClick(true)
+                },
+                modifier = Modifier.align(Alignment.CenterHorizontally),
                 profileImage = profileEditState.profile
             )
             Spacer(modifier = Modifier.height(48.dp))
--- feature/onboarding/src/main/java/com/terning/feature/onboarding/signup/SignUpRoute.kt
+++ feature/onboarding/src/main/java/com/terning/feature/onboarding/signup/SignUpRoute.kt
@@ -29,7 +29,6 @@ import com.terning.core.designsystem.component.button.RectangleButton
 import com.terning.core.designsystem.component.item.ProfileWithPlusButton
 import com.terning.core.designsystem.component.textfield.NameTextField
 import com.terning.core.designsystem.extension.addFocusCleaner
-import com.terning.core.designsystem.extension.noRippleClickable
 import com.terning.core.designsystem.extension.toast
 import com.terning.core.designsystem.theme.Grey500
 import com.terning.core.designsystem.theme.TerningPointTheme
@@ -133,9 +132,7 @@ fun SignUpScreen(
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             ProfileWithPlusButton(
-                modifier = Modifier.noRippleClickable {
-                    onProfileEditClick(true)
-                },
+                onClick = { onProfileEditClick(true) },
                 profileImage = state.profileImage
             )
         }
