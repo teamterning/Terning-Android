387f9113ee9d7b72b2c9d6cd3ce47e0c4bdd45ac
LEE YOU BIN <youbeene3409@naver.com>
2025-11-10 02:32:23 +0900

    [FEAT/#411] 새롭게 다이얼로그 구현

--- feature/splash/src/main/java/com/terning/feature/splash/component/TerningServerNoticeDialog.kt
+++ feature/splash/src/main/java/com/terning/feature/splash/component/TerningServerNoticeDialog.kt
@@ -10,18 +10,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.terning.core.designsystem.component.dialog.NoticeDialogButton
-import com.terning.core.designsystem.component.dialog.TerningNoticeDialog
 import com.terning.core.designsystem.theme.Back
 import com.terning.core.designsystem.theme.Black
 import com.terning.core.designsystem.theme.Grey150
 import com.terning.core.designsystem.theme.Grey200
 import com.terning.core.designsystem.theme.Grey350
+import com.terning.core.designsystem.theme.Grey400
 import com.terning.core.designsystem.theme.Grey500
 import com.terning.core.designsystem.theme.TerningMain
 import com.terning.core.designsystem.theme.TerningMain2
@@ -35,52 +40,75 @@ internal fun TerningServerNoticeDialog(
     onDismissButtonClick: () -> Unit,
     onDetailButtonClick: () -> Unit,
 ) {
-    TerningNoticeDialog(
-        titleText = stringResource(R.string.dialog_server_title),
-        bodyText = stringResource(R.string.dailog_bodytitle),
+    Dialog(
+        onDismissRequest = { },
+        properties = DialogProperties(
+            dismissOnBackPress = false,
+            dismissOnClickOutside = false,
+            usePlatformDefaultWidth = false,
+        )
     ) {
         Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .clip(RoundedCornerShape(5.dp))
-                .background(Back)
-                .padding(
-                    vertical = 18.dp,
-                    horizontal = 58.dp
+                .padding(horizontal = 28.dp)
+                .background(color = White, shape = RoundedCornerShape(20.dp))
+                .padding(12.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.dialog_server_title),
+                style = TerningTheme.typography.title2,
+                color = Grey500,
+                modifier = Modifier.padding(top = 20.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = stringResource(R.string.dialog_bodytitle),
+                style = TerningTheme.typography.body4.copy(textAlign = TextAlign.Center),
+                overflow = TextOverflow.Clip,
+                color = Grey400,
+            )
+            Spacer(modifier = Modifier.height(26.dp))
+            Column(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(Back)
+                    .padding(
+                        vertical = 18.dp,
+                        horizontal = 58.dp
+                    )
+            ) {
+                Text(
+                    text = stringResource(R.string.dialog_server_over_title),
+                    style = TerningTheme.typography.body6,
+                    color = Black,
                 )
-        ) {
-            Text(
-                text = stringResource(R.string.dialog_server_over_title),
-                style = TerningTheme.typography.body6,
-                color = Black,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = stringResource(R.string.dialog_server_over_day),
-                style = TerningTheme.typography.detail4,
-                color = Grey500
-            )
-        }
-        Spacer(modifier = Modifier.height(26.dp))
-        Row(
-            modifier = it,
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            NoticeDialogButton(
-                text = stringResource(R.string.dialog_dismiss),
-                contentColor = Grey350,
-                pressedContainerColor = Grey200,
-                containerColor = Grey150,
-                onClick = onDismissButtonClick,
-                modifier = Modifier.weight(1f)
-            )
-            NoticeDialogButton(
-                text = stringResource(R.string.dialog_detail),
-                contentColor = White,
-                pressedContainerColor = TerningMain2,
-                containerColor = TerningMain,
-                onClick = onDetailButtonClick,
-                modifier = Modifier.weight(1f)
-            )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = stringResource(R.string.dialog_server_over_day),
+                    style = TerningTheme.typography.detail4,
+                    color = Grey500
+                )
+            }
+            Spacer(modifier = Modifier.height(26.dp))
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                NoticeDialogButton(
+                    text = stringResource(R.string.dialog_dismiss),
+                    contentColor = Grey350,
+                    pressedContainerColor = Grey200,
+                    containerColor = Grey150,
+                    onClick = onDismissButtonClick,
+                    modifier = Modifier.weight(1f)
+                )
+                NoticeDialogButton(
+                    text = stringResource(R.string.dialog_detail),
+                    contentColor = White,
+                    pressedContainerColor = TerningMain2,
+                    containerColor = TerningMain,
+                    onClick = onDetailButtonClick,
+                    modifier = Modifier.weight(1f)
+                )
+            }
         }
     }
 }
